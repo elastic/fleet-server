6a07a6b31790f4b64a36baedef92106d8ff62b4b
Aleksandr Maus <aleksandr.maus@elastic.co>
2020-11-30 18:59:24 -0500

    Schema based generated mappings for all indicies in the .fleet schema. Fix few places in the schema

--- internal/pkg/esboot/bootstrap.go
+++ internal/pkg/esboot/bootstrap.go
@@ -9,130 +9,18 @@ import (
 // Temporary ES indices bootstrapping until we move this logic to a proper place
 // The plans at the moment possibly handle at ES plugin
 
-const (
-	agentMapping = `
-	{
-		"properties" : {
-		  "agent" : {
-			"properties": {
-			  "id" : {
-				"type" : "keyword"
-			  },
-			  "version" : {
-				"type" : "keyword"
-			  }
-			}
-		  },
-		  "action_seq_no" : {
-			"type" : "integer"
-		  },
-		  "local_metadata" : {
-			"type" :"object"
-		  },
-		  "access_api_key" : {
-			"type" : "keyword"
-		  },
-		  "access_api_key_id" : {
-			"type" : "keyword"
-		  },
-		  "policy_id" : {
-			"type" : "keyword"
-		  },
-		  "policy_revision" : {
-			"type" : "integer"
-		  },
-		  "enrolled_at": {
-			  "type": "date"
-		  },
-		  "unenrolled_at": {
-			  "type": "date"
-		  },
-		  "enrollment_started_at": {
-			  "type": "date"
-		  },
-		  "unenrollment_started_at": {
-			  "type": "date"
-		  },
-		  "upgraded_at": {
-			  "type": "date"
-		  },
-		  "upgrade_started_at": {
-			  "type": "date"
-		  },
-		  "updated_at": {
-			  "type": "date"
-		  }
-		}
-	  }
-	`
-
-	actionsMapping = `
-	{
-		"properties" : {
-		"data" : {
-			"dynamic" : "false",
-			"type": "object",
-			"enabled": "false"
-		},
-		"@timestamp" : {
-			"type" : "date"
-		},
-		"expiration" : {
-			"type" : "date"
-		},
-		"id" : {
-			"type" : "keyword"
-		},
-		"agents" : {
-			"type" : "keyword"
-		},
-		"type" : {
-			"type" : "keyword"
-		},
-		"application" : {
-			"type" : "keyword"
-		},
-		"route" : {
-			"type" : "keyword"
-		}
-		}
-	}	
-	`
-
-	resultsMapping = `
-	{
-		"properties" : {
-		"@timestamp" : {
-			"type" : "date"
-		},
-		"agent" : {
-			"type" : "keyword"
-		},
-		"action" : {
-			"type" : "keyword"
-		},
-		"errors":  {
-			"type" : "keyword"
-		},
-		"data" : {
-			"dynamic" : "false",
-			"type": "object",
-			"enabled": "false"
-		}
-		}
-	}	
-	`
-)
-
 type indexConfig struct {
 	mapping    string
 	datastream bool
 }
 
 var indexConfigs = map[string]indexConfig{
-	".fleet-agents":          {mapping: agentMapping},
-	".fleet-actions":         {mapping: actionsMapping},
-	".fleet-actions-results": {mapping: resultsMapping, datastream: true},
+	".fleet-servers":         {mapping: kMappingServer},
+	".fleet-policies":        {mapping: kMappingPolicy},
+	".fleet-policies-leader": {mapping: kMappingPolicyLeader},
+	".fleet-agents":          {mapping: kMappingAgent},
+	".fleet-actions":         {mapping: kMappingAction},
+	".fleet-actions-results": {mapping: kMappingActionResult, datastream: true},
 }
 
 // Bootstrap creates .fleet-actions data stream
