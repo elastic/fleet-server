f2841a599857eaedfa1163ea2c6dcc2f814cab9b
Blake Rouse <blake.rouse@elastic.co>
2021-03-05 08:52:51 -0500

    Add API key invalidation on unenroll ACK. (#124) (#125)
    * Add API key invalidation on unenroll ACK.
    
    * Fix import location.
    
    * Fix test.
    
    (cherry picked from commit dc762b5a39500afb68d969d43e6de83656ddbff7)

---
+++ internal/pkg/apikey/invalidate.go
@@ -0,0 +1,50 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package apikey
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+)
+
+// Invalidate invalidates the provides API keys by ID.
+func Invalidate(ctx context.Context, client *elasticsearch.Client, ids ...string) error {
+
+	payload := struct {
+		IDs []string `json:"ids,omitempty"`
+	}{
+		ids,
+	}
+
+	body, err := json.Marshal(&payload)
+	if err != nil {
+		return err
+	}
+
+	opts := []func(*esapi.SecurityInvalidateAPIKeyRequest){
+		client.Security.InvalidateAPIKey.WithContext(ctx),
+	}
+
+	res, err := client.Security.InvalidateAPIKey(
+		bytes.NewReader(body),
+		opts...,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.IsError() {
+		return fmt.Errorf("fail InvalidateAPIKey: %s", res.String())
+	}
+	return nil
+}
