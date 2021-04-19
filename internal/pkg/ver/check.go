df6eb877c04ac6d44fc304ebd2fb12d6193d0ea1
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-04-19 13:45:58 -0400

    Validate Elasticsearch version for compatibility (#249)

---
+++ internal/pkg/ver/check.go
@@ -0,0 +1,87 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package ver
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	esh "github.com/elastic/fleet-server/v7/internal/pkg/es"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/hashicorp/go-version"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	ErrUnsupportedVersion = errors.New("unsupported version")
+	ErrMalformedVersion   = errors.New("malformed version")
+)
+
+func CheckCompatibility(ctx context.Context, esCli *elasticsearch.Client, fleetVersion string) error {
+	log.Debug().Str("fleet_version", fleetVersion).Msg("check version compatibility with elasticsearch")
+
+	esVersion, err := esh.FetchESVersion(ctx, esCli)
+
+	if err != nil {
+		log.Error().Err(err).Msg("failed to fetch elasticsearch version")
+		return err
+	}
+	log.Debug().Str("elasticsearch_version", esVersion).Msg("fetched elasticsearch version")
+
+	return checkCompatibility(fleetVersion, esVersion)
+}
+
+func checkCompatibility(fleetVersion, esVersion string) error {
+	verConst, err := buildVersionConstraint(fleetVersion)
+	if err != nil {
+		log.Error().Err(err).Str("fleet_version", fleetVersion).Msg("failed to build constraint")
+		return err
+	}
+
+	ver, err := parseVersion(esVersion)
+	if err != nil {
+		return err
+	}
+
+	if !verConst.Check(ver) {
+		log.Error().Err(ErrUnsupportedVersion).Msg("failed elasticsearch version check")
+		return ErrUnsupportedVersion
+	}
+	log.Info().Str("fleet_version", fleetVersion).Str("elasticsearch_version", esVersion).Msg("versions are compatible")
+	return nil
+}
+
+func buildVersionConstraint(fleetVersion string) (version.Constraints, error) {
+	ver, err := parseVersion(fleetVersion)
+	if err != nil {
+		return nil, err
+	}
+	return version.NewConstraint(fmt.Sprintf(">= %s", minimizePatch(ver)))
+}
+
+func minimizePatch(ver *version.Version) string {
+	segments := ver.Segments()
+	if len(segments) > 2 {
+		segments = segments[:2]
+	}
+	segments = append(segments, 0)
+	segStrs := make([]string, 0, len(segments))
+	for _, segment := range segments {
+		segStrs = append(segStrs, strconv.Itoa(segment))
+	}
+	return strings.Join(segStrs, ".")
+}
+
+func parseVersion(sver string) (*version.Version, error) {
+	ver, err := version.NewVersion(sver)
+	if err != nil {
+		return nil, fmt.Errorf("%v: %w", err, ErrMalformedVersion)
+	}
+	return ver, nil
+}
---
+++ internal/pkg/ver/check_test.go
@@ -0,0 +1,92 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package ver
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestCheckCompatibilityInternal(t *testing.T) {
+	tests := []struct {
+		name         string
+		fleetVersion string
+		esVersion    string
+		err          error
+	}{
+		{
+			name:         "empty fleet and elasticsearch version",
+			fleetVersion: "",
+			esVersion:    "",
+			err:          ErrMalformedVersion,
+		},
+		{
+			name:         "empty fleet version",
+			fleetVersion: "",
+			esVersion:    "8.0.0",
+			err:          ErrMalformedVersion,
+		},
+		{
+			name:         "empty elasticsearch version",
+			fleetVersion: "7.13",
+			esVersion:    "",
+			err:          ErrMalformedVersion,
+		},
+		{
+			name:         "supported elasticsearch 713-713",
+			fleetVersion: "7.13.0",
+			esVersion:    "7.13.0",
+			err:          nil,
+		},
+		{
+			name:         "supported elasticsearch 7131-7132",
+			fleetVersion: "7.13.2",
+			esVersion:    "7.13.1",
+			err:          nil,
+		},
+		{
+			name:         "supported elasticsearch 713-714",
+			fleetVersion: "7.13.2",
+			esVersion:    "7.14.2",
+			err:          nil,
+		},
+		{
+			name:         "supported elasticsearch 715-800",
+			fleetVersion: "7.15.2",
+			esVersion:    "8.0.0",
+			err:          nil,
+		},
+		{
+			name:         "unsupported elasticsearch 714-713",
+			fleetVersion: "7.14.0",
+			esVersion:    "7.13.1",
+			err:          ErrUnsupportedVersion,
+		},
+		{
+			name:         "unsupported elasticsearch 800-718",
+			fleetVersion: "8.0.0",
+			esVersion:    "7.18.0",
+			err:          ErrUnsupportedVersion,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := checkCompatibility(tc.fleetVersion, tc.esVersion)
+			if tc.err != nil {
+				if err == nil {
+					t.Error("expected error")
+				} else {
+					if !errors.Is(err, tc.err) {
+						t.Errorf("unexpected error kind: %v", err)
+					}
+				}
+			} else {
+				if err != nil {
+					t.Error("unexpected error")
+				}
+			}
+		})
+	}
+}
