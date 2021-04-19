881f51445cb95b3b42050854a8c9d25235500374
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-04-19 12:17:38 -0400

    Validate Elasticsearch version for compatibility

---
+++ internal/pkg/es/info.go
@@ -0,0 +1,50 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package es
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+
+	"github.com/elastic/go-elasticsearch/v8"
+)
+
+type versionInfo struct {
+	Number string `json:"number"`
+}
+
+type infoResponse struct {
+	Version versionInfo `json:"version"`
+	Error   ErrorT      `json:"error,omitempty"`
+}
+
+func FetchESVersion(ctx context.Context, esCli *elasticsearch.Client) (version string, err error) {
+	res, err := esCli.Info(
+		esCli.Info.WithContext(ctx),
+	)
+
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	var sres infoResponse
+
+	err = json.NewDecoder(res.Body).Decode(&sres)
+	if err != nil {
+		return
+	}
+
+	// Check error
+	err = TranslateError(res.StatusCode, sres.Error)
+	if err != nil {
+		return
+	}
+
+	verStr := strings.TrimSpace(strings.TrimSuffix(strings.ToLower(sres.Version.Number), "-snapshot"))
+
+	return verStr, nil
+}
