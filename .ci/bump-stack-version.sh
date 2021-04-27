bb32dd76e96aab608d60031e42e27313b4f1aeff
Victor Martinez <victormartinezrubio@gmail.com>
2021-04-27 12:42:48 +0100

    [mergify]: delete branch related to automation after it gets merged and commit messages (#276)
    (cherry picked from commit 541def17912865ad74828eec5c150617a0d75691)

--- .ci/bump-stack-version.sh
+++ .ci/bump-stack-version.sh
@@ -29,12 +29,13 @@ ${SED} -E -e "s#(ELASTICSEARCH_VERSION)=[0-9]+\.[0-9]+\.[0-9]+(-[a-f0-9]{8})?#\1
 
 echo "Commit changes"
 if [ "$CREATE_BRANCH" = "true" ]; then
-	git checkout -b "update-stack-version-$(date "+%Y%m%d%H%M%S")"
+	base=$(git rev-parse --abbrev-ref HEAD | sed 's#/#-#g')
+	git checkout -b "update-stack-version-$(date "+%Y%m%d%H%M%S")-${base}"
 else
 	echo "Branch creation disabled."
 fi
 git add dev-tools/integration/.env
-git diff --staged --quiet || git commit -m "bump stack version ${VERSION}"
+git diff --staged --quiet || git commit -m "[Automation] Update elastic stack version to ${VERSION} for testing"
 git --no-pager log -1
 
 echo "You can now push and create a Pull Request"
