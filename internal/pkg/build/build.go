8842ed9169a3296d6b63d6384c5795f6e4eecc85
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-08-23 15:00:54 -0400

    Set User-Agent in HTTP requests

---
+++ internal/pkg/build/build.go
@@ -0,0 +1,20 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package build
+
+import "time"
+
+type Info struct {
+	Version, Commit string
+	BuildTime       time.Time
+}
+
+func Time(stime string) time.Time {
+	t, err := time.Parse(time.RFC3339, stime)
+	if err != nil {
+		return time.Time{}
+	}
+	return t
+}
