85bb817036f5fbe01a0b79bff62f94f245204351
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-09-01 08:13:45 -0400

    Encapsulate the agent new version safe check, full unit test coverage

--- internal/pkg/model/ext.go
+++ internal/pkg/model/ext.go
@@ -25,3 +25,17 @@ func (m *Server) Time() (time.Time, error) {
 func (m *Server) SetTime(t time.Time) {
 	m.Timestamp = t.Format(time.RFC3339Nano)
 }
+
+// GetNewVersion returns Agent version if it is different from ver, otherwise return empty string
+func (m *Agent) GetNewVersion(ver string) string {
+	if m == nil {
+		return ""
+	}
+
+	var newVer string
+	if m.Agent == nil || ver != m.Agent.Version {
+		newVer = ver
+	}
+
+	return newVer
+}
---
+++ internal/pkg/model/ext_test.go
@@ -0,0 +1,86 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// +build !integration
+
+package model
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestAgentGetNewVersion(t *testing.T) {
+	tests := []struct {
+		Name    string
+		Agent   *Agent
+		Ver     string
+		WantVer string
+	}{
+		{
+			Name: "nil",
+		},
+		{
+			Name:  "agent no meta empty version",
+			Agent: &Agent{},
+		},
+		{
+			Name:    "agent no meta nonempty version",
+			Agent:   &Agent{},
+			Ver:     "7.14",
+			WantVer: "7.14",
+		},
+		{
+			Name: "agent with meta empty new version",
+			Agent: &Agent{
+				Agent: &AgentMetadata{
+					Version: "7.14",
+				},
+			},
+			Ver:     "",
+			WantVer: "",
+		},
+		{
+			Name: "agent with meta empty version",
+			Agent: &Agent{
+				Agent: &AgentMetadata{
+					Version: "",
+				},
+			},
+			Ver:     "7.15",
+			WantVer: "7.15",
+		},
+		{
+			Name: "agent with meta non empty version",
+			Agent: &Agent{
+				Agent: &AgentMetadata{
+					Version: "7.14",
+				},
+			},
+			Ver:     "7.14",
+			WantVer: "",
+		},
+		{
+			Name: "agent with meta new version",
+			Agent: &Agent{
+				Agent: &AgentMetadata{
+					Version: "7.14",
+				},
+			},
+			Ver:     "7.15",
+			WantVer: "7.15",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
+			newVer := tc.Agent.GetNewVersion(tc.Ver)
+			diff := cmp.Diff(tc.WantVer, newVer)
+			if diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
