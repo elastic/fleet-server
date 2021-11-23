b56a25868403d55ba13ed29763a7b622528f4e7d
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-11-23 06:36:13 -0500

    Improve expired actions cleanup, use _delete_by_query instead (#906)
    * Improve expired actions cleanup, use _delete_by_query instead
    
    (cherry picked from commit fae23a3bc74aea331fc919facaec9e6bf1992483)

--- internal/pkg/gc/actions.go
+++ internal/pkg/gc/actions.go
@@ -6,144 +6,79 @@ package gc
 
 import (
 	"context"
-	"math/rand"
-	"net/http"
-	"time"
+	"strconv"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/elastic/fleet-server/v7/internal/pkg/bulk"
 	"github.com/elastic/fleet-server/v7/internal/pkg/dl"
-	"github.com/elastic/fleet-server/v7/internal/pkg/es"
 	"github.com/elastic/fleet-server/v7/internal/pkg/scheduler"
-	"github.com/elastic/fleet-server/v7/internal/pkg/wait"
-)
-
-const (
-	defaultMaxWaitInCleanupLoop         = 10 * time.Second // the wait in the cleanup loop between iteration is random
-	defaultActionsSelectSize            = 1000
-	defaultActionsCleanupBeforeInterval = 24 * 30 * time.Hour
 )
 
 type ActionsCleanupConfig struct {
-	maxWaitInCleanupLoop  time.Duration
-	actionsSelectSize     int
-	cleanupBeforeInterval time.Duration
+	cleanupIntervalAfterExpired string
 }
 
 type ActionsCleanupOpt func(c *ActionsCleanupConfig)
 
-func WithMaxWaitInCleanupLoop(maxWaitInCleanupLoop time.Duration) ActionsCleanupOpt {
+// isIntervalStringValid validated interval string according to the elasticsearch documentation
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/common-options.html#date-math
+func isIntervalStringValid(interval string) bool {
+	if len(interval) < 2 {
+		return false
+	}
+	if strings.HasPrefix(interval, "-") {
+		return false
+	}
+
+	num := interval[0 : len(interval)-1]
+	suffix := interval[len(num):]
+
+	switch suffix {
+	case "y", "M", "w", "d", "h", "H", "m", "s":
+		if _, err := strconv.Atoi(num); err == nil {
+			return true
+		}
+	}
+
+	return false
+}
+
+func WithCleanupIntervalAfterExpired(cleanupIntervalAfterExpired string) ActionsCleanupOpt {
 	return func(c *ActionsCleanupConfig) {
-		c.maxWaitInCleanupLoop = maxWaitInCleanupLoop
+		// Use the interval if valid, otherwise keep the default
+		if isIntervalStringValid(cleanupIntervalAfterExpired) {
+			c.cleanupIntervalAfterExpired = cleanupIntervalAfterExpired
+		}
 	}
 }
 
-func WithActionSelectSize(actionsSelectSize int) ActionsCleanupOpt {
-	return func(c *ActionsCleanupConfig) {
-		c.actionsSelectSize = actionsSelectSize
-	}
-}
-
-func WithActionCleanupBeforeInterval(cleanupBeforeInterval time.Duration) ActionsCleanupOpt {
-	return func(c *ActionsCleanupConfig) {
-		c.cleanupBeforeInterval = cleanupBeforeInterval
-	}
-}
-
-func getActionsGCFunc(bulker bulk.Bulk, cleanupBeforeInterval time.Duration) scheduler.WorkFunc {
+func getActionsGCFunc(bulker bulk.Bulk, cleanupIntervalAfterExpired string) scheduler.WorkFunc {
 	return func(ctx context.Context) error {
 		return cleanupActions(ctx, dl.FleetActions, bulker,
-			WithActionCleanupBeforeInterval(cleanupBeforeInterval))
+			WithCleanupIntervalAfterExpired(cleanupIntervalAfterExpired))
 	}
 }
 
 func cleanupActions(ctx context.Context, index string, bulker bulk.Bulk, opts ...ActionsCleanupOpt) error {
-	log := log.With().Str("ctx", "fleet actions cleanup").Logger()
-
 	c := ActionsCleanupConfig{
-		cleanupBeforeInterval: defaultActionsCleanupBeforeInterval,
-		actionsSelectSize:     defaultActionsSelectSize,
-		maxWaitInCleanupLoop:  defaultMaxWaitInCleanupLoop,
+		cleanupIntervalAfterExpired: defaultCleanupIntervalAfterExpired,
 	}
 
 	for _, opt := range opts {
 		opt(&c)
 	}
 
-	// Cleanup expired actions where expired timetamp is older than current time minus cleanupBeforeInterval
-	// Example: cleanup up actions that expired more than two weeks ago
-	expiredBefore := time.Now().Add(-c.cleanupBeforeInterval)
+	log := log.With().Str("ctx", "fleet actions cleanup").Str("interval", "now-"+c.cleanupIntervalAfterExpired).Logger()
 
-	// Random generator for calculating random pause duration in the cleanup loop
-	r := rand.New(rand.NewSource(time.Now().Unix()))
+	log.Debug().Msg("delete expired actions")
 
-	var (
-		hits []es.HitT
-		err  error
-	)
-
-	for {
-		log.Debug().Str("expired_before", expiredBefore.UTC().Format(time.RFC3339)).Msgf("find actions that expired before given date/time")
-		hits, err = dl.FindExpiredActionsHitsForIndex(ctx, index, bulker, expiredBefore, c.actionsSelectSize)
-		if err != nil {
-			return err
-		}
-
-		if len(hits) == 0 {
-			log.Debug().Msg("no more expired actions found, done cleaning")
-			return nil
-		}
-
-		log.Debug().Int("count", len(hits)).Msg("delete expired actions")
-		if len(hits) > 0 {
-			ops := make([]bulk.MultiOp, len(hits))
-			for i := 0; i < len(hits); i++ {
-				ops[i] = bulk.MultiOp{Index: index, Id: hits[i].Id}
-			}
-
-			res, err := bulker.MDelete(ctx, ops)
-			if err != nil {
-				// The error is logged
-				log.Debug().Err(err).Msg("failed to delete actions")
-				if eserr, ok := err.(*es.ErrElastic); ok {
-					if eserr.Status == http.StatusNotFound {
-						err = nil
-					}
-				}
-				if err != nil {
-					return err
-				}
-			}
-			for i, r := range res {
-				if r.Error != nil {
-					err = es.TranslateError(r.Status, r.Error)
-					if err != nil {
-						log.Debug().Err(err).Str("action_id", hits[i].Id).Msg("failed to delete action")
-						if r.Status != http.StatusNotFound {
-							return err
-						}
-					}
-				}
-			}
-		}
-
-		// If number of records selected is less than max, can exit the cleanup loop
-		if len(hits) < c.actionsSelectSize {
-			return nil
-		}
-
-		// The full number of hits was returned
-		// Can potentially have more records
-		// Pause before doing another iteration
-		if c.maxWaitInCleanupLoop > 0 {
-			pauseDuration := time.Duration(r.Int63n(int64(c.maxWaitInCleanupLoop)))
-			log.Debug().Dur("pause_duration", pauseDuration).Msg("more actions could be avaiable, pause before the next cleanup cycle")
-			// Wait with context some random short interval to avoid tight loops
-			err = wait.WithContext(ctx, pauseDuration)
-			if err != nil {
-				return err
-			}
-		}
+	deleted, err := dl.DeleteExpiredForIndex(ctx, index, bulker, c.cleanupIntervalAfterExpired)
+	if err != nil {
+		log.Debug().Err(err).Msg("failed to delete actions")
+		return err
 	}
+	log.Debug().Int64("count", deleted).Msg("deleted expired actions")
+	return nil
 }
