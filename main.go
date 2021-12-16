172bb1ea70ccf1da4a00f60528979d8a81288986
Nicolas Ruflin <spam@ruflin.com>
2021-12-16 12:48:06 +0100

    [Release] Update release version to 7.17.0 (#1002)

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/elastic/fleet-server/v7/internal/pkg/build"
 )
 
-const defaultVersion = "7.16.2"
+const defaultVersion = "7.17.0"
 
 var (
 	Version   string = defaultVersion
