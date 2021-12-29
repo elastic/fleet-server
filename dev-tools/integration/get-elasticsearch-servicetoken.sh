5bea28535fbafc6450cd3366926f155820000744
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-12-28 21:24:42 -0500

    Remove .fleet-* mapping cruft. Test rely on .fleet indicies plugin to boostrap indicies/datastreams appropriately.

--- dev-tools/integration/get-elasticsearch-servicetoken.sh
+++ dev-tools/integration/get-elasticsearch-servicetoken.sh
@@ -7,7 +7,7 @@ jsonBody="$(curl -sSL -XPOST "$host/_security/service/elastic/fleet-server/crede
 # use grep and sed to get the service token value as we may not have jq or a similar tool on the instance
 token=$(echo ${jsonBody} |  grep -Eo '"value"[^}]*' | grep -Eo ':.*' | sed -r "s/://" | sed -r 's/"//g')
 
-# cache or use cashed token in order to be able to run repeative integration tests,
+# cache or use cached token in order to be able to run repeative integration tests,
 # very useful during development, without recreating elasticsearch instance every time.
 if [ -z "$token" ]
 then
