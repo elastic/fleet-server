431fe1d302e2db21165a5934c1d0a107ea4a6abd
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-23 11:38:47 +0000

    dra: slack/email with the branch

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -146,7 +146,7 @@ pipeline {
     }
     failure {
       echo 'disabled'
-    // notifyStatus(slackStatus: 'danger', subject: "[${env.REPO}] DRA failed", body: "Build: (<${env.RUN_DISPLAY_URL}|here>)")
+    // notifyStatus(slackStatus: 'danger', subject: "[${env.REPO}@${env.BRANCH_NAME}] DRA failed", body: "Build: (<${env.RUN_DISPLAY_URL}|here>)")
     }
   }
 }
