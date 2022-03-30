2fff22c4e0b1403c461a1b9161fcf45cedf94a8f
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-30 14:59:18 +0100

    ci: rename file

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -142,7 +142,7 @@ pipeline {
             expression { return env.IS_BRANCH_AVAILABLE == "true" }
           }
           environment {
-            DRA_OUTPUT = 'release-manager-report.out'
+            DRA_OUTPUT = 'release-manager-report.txt'
           }
           steps {
             googleStorageDownload(bucketUri: "gs://${JOB_GCS_BUCKET}/${URI_SUFFIX}/*",
