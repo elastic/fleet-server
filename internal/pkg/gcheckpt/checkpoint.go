fbd6986e91243971d9415b92031fae5363db6826
michel-laterman <michel.laterman@elastic.co>
2022-05-06 10:32:55 -0500

    fix typo

--- internal/pkg/gcheckpt/checkpoint.go
+++ internal/pkg/gcheckpt/checkpoint.go
@@ -3,7 +3,7 @@
 // you may not use this file except in compliance with the Elastic License.
 
 // Package gcheckpt handles the fleet API's global_checkpoints operations
-// checkpoints are used to track which actions, agetnc (docs in general) have been read based on the seqno recieved.
+// checkpoints are used to track which actions, agetnc (docs in general) have been read based on the seqno received.
 package gcheckpt
 
 import (
