3860378e2c5f31b36bbec5c626dfe899c9cbb113
Julia Bardi <julia.bardi@elastic.co>
2022-09-27 15:28:15 +0200

    Revert "updating upgrade_status: completed (#1833)"
    This reverts commit 23be42ad5da92250a4f62e762b4971c357e7ae15.

--- internal/pkg/dl/constants.go
+++ internal/pkg/dl/constants.go
@@ -50,7 +50,6 @@ const (
 	FieldUnenrolledAt     = "unenrolled_at"
 	FieldUpgradedAt       = "upgraded_at"
 	FieldUpgradeStartedAt = "upgrade_started_at"
-	FieldUpgradeStatus    = "upgrade_status"
 
 	FieldDecodedSha256 = "decoded_sha256"
 	FieldIdentifier    = "identifier"
