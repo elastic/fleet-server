14f2557a4283a0d0e14d2581e9ab9cb73e15c311
michel-laterman <michel.laterman@elastic.co>
2022-12-08 13:57:43 -0800

    fix linter

--- internal/pkg/es/fleet_global_checkpoints.go
+++ internal/pkg/es/fleet_global_checkpoints.go
@@ -123,7 +123,7 @@ func (r GlobalCheckpointsRequest) Do(ctx context.Context, transport esapi.Transp
 	// NOTE do not close the response body here, it's handled further along (gcheckpts)
 	// Closing the body within this method will cause operations that use checkpoints to fail
 	// a lot of operations use checkpoints
-	res, err := transport.Perform(req) //nolint:bodyclose // :(
+	res, err := transport.Perform(req)
 	if err != nil {
 		return nil, err
 	}
