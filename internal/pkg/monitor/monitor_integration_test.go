2b2d8e27e2114c619d41be9f2886507c5a10c054
Aleksandr Maus <aleksandr.maus@elastic.co>
2023-01-04 08:17:45 -0500

    Fix the index monitor checkpoint tracking for when the documents are deleted (#2206)
    * Fix the index monitor checkpoint tracking for when the documents are deleted
    
    * Make linter happy
    
    * Refactor unit tests, hopefully makes SonarCloud happier
    
    * Make linter happy
    
    * Addressed code review comment
    
    * Add changelog entry, fix one comment typo in the code
    
    (cherry picked from commit ec38a94b03a84e4050fc7c98e732919d6904a8f9)
    
    # Conflicts:
    #	CHANGELOG.next.asciidoc

--- internal/pkg/monitor/monitor_integration_test.go
+++ internal/pkg/monitor/monitor_integration_test.go
@@ -9,17 +9,23 @@ package monitor
 
 import (
 	"context"
-	"errors"
 	"sync"
 	"testing"
+	"time"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/elastic/fleet-server/v7/internal/pkg/bulk"
 	"github.com/elastic/fleet-server/v7/internal/pkg/dl"
+	"github.com/elastic/fleet-server/v7/internal/pkg/gcheckpt"
 	"github.com/elastic/fleet-server/v7/internal/pkg/model"
+	"github.com/elastic/fleet-server/v7/internal/pkg/sleep"
+	"github.com/elastic/fleet-server/v7/internal/pkg/sqn"
+
 	ftesting "github.com/elastic/fleet-server/v7/internal/pkg/testing"
+	testlog "github.com/elastic/fleet-server/v7/internal/pkg/testing/log"
 )
 
 func TestSimpleMonitorEmptyIndex(t *testing.T) {
@@ -27,6 +33,7 @@ func TestSimpleMonitorEmptyIndex(t *testing.T) {
 	defer cn()
 
 	index, bulker := ftesting.SetupCleanIndex(ctx, t, dl.FleetActions)
+
 	runSimpleMonitorTest(t, ctx, index, bulker)
 }
 
@@ -35,77 +42,192 @@ func TestSimpleMonitorNonEmptyIndex(t *testing.T) {
 	defer cn()
 
 	index, bulker, _ := ftesting.SetupActions(ctx, t, 1, 12)
+
 	runSimpleMonitorTest(t, ctx, index, bulker)
 }
 
-func runSimpleMonitorTest(t *testing.T, ctx context.Context, index string, bulker bulk.Bulk) {
+func TestSimpleMonitorCheckpointOutOfSync(t *testing.T) {
+	log := testlog.SetLogger(t)
+	ctx, cn := context.WithCancel(context.Background())
+	defer cn()
+
+	index, bulker, _ := ftesting.SetupActions(ctx, t, 1, 12)
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	var createdActions []model.Action
+	ch := make(chan model.Action)
 	readyCh := make(chan error)
 	mon, err := NewSimple(index, bulker.Client(), bulker.Client(),
 		WithReadyChan(readyCh),
 	)
 	require.NoError(t, err)
 
-	// Start monitor
 	var wg sync.WaitGroup
-	mctx, mcn := context.WithCancel(ctx)
-	var merr error
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		merr = mon.Run(mctx)
-		if errors.Is(merr, context.Canceled) {
-			merr = nil
+	g.Go(func() error {
+		return runSimpleMonitor(t, ctx, mon, readyCh, ch, func(ctx context.Context) error {
+			defer wg.Done()
+			var err error
+			createdActions, err = ftesting.StoreRandomActions(ctx, bulker, index, 1, 7)
+			return err
+		})
+	})
+
+	g.Go(func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case <-ch:
+			}
 		}
-	}()
+	})
 
-	// Wait until monitor is running
-	err = <-readyCh
+	// Wait until actions are created
+	wg.Wait()
+
+	var checkpoint, monCheckpoint sqn.SeqNo
+	checkpoint, err = gcheckpt.Query(ctx, bulker.Client(), index)
 	require.NoError(t, err)
 
-	// Create random actions
-	actions, err := ftesting.StoreRandomActions(ctx, bulker, index, 1, 7)
+	log.Debug().Int64("checkpoint", checkpoint.Value()).Msg("checkpoint before test action delete")
+
+	// Delete an action to emulate the gap between the fleet server tracking checkpoint and the index checkpoint
+	// The delete causes the checkpoint increment and the fleet-server was not updating it's checkpoint tracked value correctly
+	// in these cases.
+	idx := len(createdActions) - 1
+	err = bulker.Delete(ctx, index, createdActions[idx].Id, bulk.WithRefresh())
 	require.NoError(t, err)
 
-	var gotActions []model.Action
-	// Listen monitor updates
-	var mwg sync.WaitGroup
-	mwg.Add(1)
-	go func() {
-		defer mwg.Done()
+	checkpoint, err = gcheckpt.Query(ctx, bulker.Client(), index)
+	require.NoError(t, err)
+	log.Debug().Int64("checkpoint", checkpoint.Value()).Msg("checkpoint after test action delete")
+
+	// Wait for fleet server monitor checkpoint to be incremented after delete
+	m, _ := mon.(*simpleMonitorT)
+	timeout := 10 * time.Second // This should not take that long, can wait until the test times out or shorter like this
+	start := time.Now()
+	for {
+		monCheckpoint = m.loadCheckpoint()
+		log.Debug().Int64("wait checkpoint", checkpoint.Value()).Int64("got checkpoint", monCheckpoint.Value()).Msg("monitor checkpoint")
+		if checkpoint.Value() == monCheckpoint.Value() {
+			break
+		}
+
+		if time.Since(start) >= timeout {
+			t.Fatal("timed out waiting for the checkpoint update")
+		}
+		err = sleep.WithContext(ctx, 100*time.Millisecond)
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, checkpoint, monCheckpoint)
+
+	// Cancel context to stop monitor and exit all go routines
+	cn()
+
+	require.NoError(t, g.Wait())
+}
+
+type onReadyFunc func(ctx context.Context) error
+
+func runNewSimpleMonitor(t *testing.T, ctx context.Context, index string, bulker bulk.Bulk, ch chan<- model.Action, onReady onReadyFunc) error {
+	t.Helper()
+	readyCh := make(chan error)
+	mon, err := NewSimple(index, bulker.Client(), bulker.Client(),
+		WithReadyChan(readyCh),
+	)
+	if err != nil {
+		return err
+	}
+	return runSimpleMonitor(t, ctx, mon, readyCh, ch, onReady)
+}
+
+func runSimpleMonitor(t *testing.T, ctx context.Context, mon SimpleMonitor, readyCh chan error, ch chan<- model.Action, onReady onReadyFunc) error {
+	t.Helper()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	monCtx, monCn := context.WithCancel(ctx)
+	defer monCn()
+
+	// Run monitor
+	g.Go(func() error {
+		return mon.Run(monCtx)
+	})
+
+	// Wait for monitor ready and call createActions
+	err := <-readyCh
+	if err != nil {
+		return err
+	}
+	if onReady != nil {
+		err = onReady(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Listen for monitor notifications, exit when received all the actions
+	g.Go(func() error {
+		// Cancel/stop monitor on exit
+		defer monCn()
 		for {
 			select {
 			case hits := <-mon.Output():
 				for _, hit := range hits {
 					var action model.Action
-					er := hit.Unmarshal(&action)
-					require.NoError(t, er)
-					gotActions = append(gotActions, action)
-					if len(gotActions) == len(actions) {
-						return
+					err := hit.Unmarshal(&action)
+					if err != nil {
+						return err
 					}
+					ch <- action
 				}
 			case <-ctx.Done():
-				return
+				return nil
 			}
 		}
-	}()
-	mwg.Wait()
+	})
+
+	return g.Wait()
+}
+
+func runSimpleMonitorTest(t *testing.T, ctx context.Context, index string, bulker bulk.Bulk) {
+	ctx, cn := context.WithCancel(context.Background())
+	defer cn()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	var createdActions []model.Action
+	ch := make(chan model.Action)
+	g.Go(func() error {
+		return runNewSimpleMonitor(t, ctx, index, bulker, ch, func(ctx context.Context) error {
+			var err error
+			createdActions, err = ftesting.StoreRandomActions(ctx, bulker, index, 1, 7)
+			return err
+		})
+	})
+
+	var gotActions []model.Action
+	g.Go(func() error {
+		defer cn()
+		for a := range ch {
+			gotActions = append(gotActions, a)
+			if len(createdActions) == len(gotActions) {
+				return nil
+			}
+		}
+		return nil
+	})
+
+	require.NoError(t, g.Wait())
 
 	// Need to set the seqno that are known only after the documents where indexed
 	// in order to compare the slices of structs as a whole
 	for i, a := range gotActions {
-		actions[i].SeqNo = a.SeqNo
+		createdActions[i].SeqNo = a.SeqNo
 	}
 
-	// The documents should be the same and in the same order
-	diff := cmp.Diff(actions, gotActions)
-	if diff != "" {
-		t.Fatal(diff)
-	}
-
-	// Stop monitor and wait for clean exit
-	mcn()
-	wg.Wait()
-	require.NoError(t, merr)
-
+	assert.Equal(t, createdActions, gotActions)
 }
