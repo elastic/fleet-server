d549e802f53ea38f8211f559d4573bc8c1d9c323
michel-laterman <michel.laterman@elastic.co>
2023-04-13 07:48:44 -0700

    Change CheckinMaxPoll default to 1h

--- internal/pkg/config/timeouts.go
+++ internal/pkg/config/timeouts.go
@@ -63,5 +63,5 @@ func (c *ServerTimeouts) InitDefaults() {
 	// MaxPoll is the maximum allowed value for a long poll when the client specified poll_timeout value is used.
 	// The long poll value is poll_timeout-2m, and the request's write timeout is set to poll_timeout-1m
 	// CheckinMaxPoll values of less then 1m are effectively ignored and a 1m limit is used.
-	c.CheckinMaxPoll = 2 * time.Hour
+	c.CheckinMaxPoll = time.Hour
 }
