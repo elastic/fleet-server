e874e7fc5b2ebea8d0f89c87c4b68f1cfa296bba
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-05-19 11:51:22 +0300

    change pre-exit hook to post-command

--- .buildkite/scripts/build_image.sh
+++ .buildkite/scripts/build_image.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 source .buildkite/scripts/common.sh
 
-trap docker_logout EXIT
-
 .buildkite/scripts/imageprep.sh build-image
 .buildkite/scripts/imageprep.sh push-image
 .buildkite/scripts/imageprep.sh retag-and-push-image
