21915c07283c41d1dc8de828f0d46aff6ea91b3a
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-05-19 11:47:05 +0300

    final test after cleanup the code

--- .buildkite/scripts/build_image.sh
+++ .buildkite/scripts/build_image.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 source .buildkite/scripts/common.sh
 
+trap docker_logout EXIT
+
 .buildkite/scripts/imageprep.sh build-image
 .buildkite/scripts/imageprep.sh push-image
 .buildkite/scripts/imageprep.sh retag-and-push-image
-
-# docker_logout
