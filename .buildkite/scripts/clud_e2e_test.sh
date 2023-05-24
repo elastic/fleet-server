06b584b1be5c86b56f4811ccb848e508fc014f94
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-05-24 14:50:59 +0300

    rallback update the docker because of permissions issue

--- .buildkite/scripts/clud_e2e_test.sh
+++ .buildkite/scripts/clud_e2e_test.sh
@@ -12,10 +12,4 @@ with_Terraform
 
 .ci/scripts/install-docker-compose.sh
 
-docker version  | grep -A 2 -E "^Client|^Server"
-
-apt-get update && apt-get install docker-ce=5:24.0.1-1~ubuntu.22.04~jammy docker-ce-cli=5:24.0.1-1~ubuntu.22.04~jammy
-
-docker version  | grep -A 2 -E "^Client|^Server"
-
 USER=fleetserverci make test-cloude2e
