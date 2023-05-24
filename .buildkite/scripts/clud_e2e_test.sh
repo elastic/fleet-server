551c493957bef60075842f300d11f62ab8540fad
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-05-24 11:47:27 +0300

    test secrets masking

--- .buildkite/scripts/clud_e2e_test.sh
+++ .buildkite/scripts/clud_e2e_test.sh
@@ -4,6 +4,21 @@ set -euo pipefail
 
 source .buildkite/scripts/common.sh
 
+#START TEST PART
+echo "TESTVAR1 = ${TESTVAR1}"
+echo "TESTVAR1 = $${TESTVAR1}"
+
+echo "TESTVAR2 = ${TESTVAR2}"
+echo "TESTVAR2 = $${TESTVAR2}"
+echo "TESTVAR2_SECRET = ${TESTVAR2_SECRET}"
+echo "TESTVAR2_SECRET = $${TESTVAR2_SECRET}"
+
+echo "TESTVAR3 = ${TESTVAR3}"
+echo "TESTVAR3 = $${TESTVAR3}"
+
+#END TEST PART
+
+
 add_bin_path
 
 with_go
