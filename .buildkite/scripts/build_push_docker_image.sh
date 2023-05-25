66191526890bc43137cb00f022f031e68f8781a3
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-05-25 11:30:00 +0300

    fix push script

--- .buildkite/scripts/build_push_docker_image.sh
+++ .buildkite/scripts/build_push_docker_image.sh
@@ -8,20 +8,19 @@ set -xv
 
 echo "Building the docker image..."
 if ! docker pull -q ${DOCKER_IMAGE}:${DOCKER_IMAGE_SHA_TAG} 2> /dev/null; then
-    DOCKER_IMAGE="${DOCKER_IMAGE}"
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_SHA_TAG}"
-    make build-docker
-    make release-docker
+    DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make build-docker
+    DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make release-docker
 fi
 
 if [ -n "${BUILDKITE_TAG}" ]; then
     docker tag "${DOCKER_IMAGE}":"${DOCKER_IMAGE_SHA_TAG}" "${DOCKER_IMAGE}":"${DOCKER_IMAGE_GIT_TAG}"
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_GIT_TAG}"
-    make release-docker
+    DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make release-docker
 else
     docker tag "${DOCKER_IMAGE}":"${DOCKER_IMAGE_SHA_TAG}" "${DOCKER_IMAGE}":"${DOCKER_IMAGE_LATEST_TAG}"
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_LATEST_TAG}"
-    make release-docker
- fi
+    DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make release-docker
+fi
 
 set +xv
