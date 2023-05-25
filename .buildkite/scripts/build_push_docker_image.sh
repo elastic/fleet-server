50153664f8ab2f2ab81c0f0d36ab31c4e9dc4e66
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-05-25 13:24:59 +0300

    add func publish_docker_image

--- .buildkite/scripts/build_push_docker_image.sh
+++ .buildkite/scripts/build_push_docker_image.sh
@@ -8,15 +8,15 @@ echo "Building the docker image..."
 if ! docker pull -q ${DOCKER_IMAGE}:${DOCKER_IMAGE_SHA_TAG} 2> /dev/null; then
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_SHA_TAG}"
     DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make build-docker
-    DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make release-docker
+    publish_docker_image
 fi
 
 if [ -n "${BUILDKITE_TAG}" ]; then
     docker tag "${DOCKER_IMAGE}":"${DOCKER_IMAGE_SHA_TAG}" "${DOCKER_IMAGE}":"${DOCKER_IMAGE_GIT_TAG}"
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_GIT_TAG}"
-    DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make release-docker
+    publish_docker_image
 else
     docker tag "${DOCKER_IMAGE}":"${DOCKER_IMAGE_SHA_TAG}" "${DOCKER_IMAGE}":"${DOCKER_IMAGE_LATEST_TAG}"
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_LATEST_TAG}"
-    DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make release-docker
+    publish_docker_image
 fi
