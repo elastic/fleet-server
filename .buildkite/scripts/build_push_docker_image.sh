50a5b730886b4c903779f0b801396b25e3d98fc9
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-05-25 11:37:14 +0300

    add cloud tests

--- .buildkite/scripts/build_push_docker_image.sh
+++ .buildkite/scripts/build_push_docker_image.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 source .buildkite/scripts/common.sh
 
-set -xv
-
 echo "Building the docker image..."
 if ! docker pull -q ${DOCKER_IMAGE}:${DOCKER_IMAGE_SHA_TAG} 2> /dev/null; then
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_SHA_TAG}"
@@ -22,5 +20,3 @@ else
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_LATEST_TAG}"
     DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make release-docker
 fi
-
-set +xv
