a4037a13e803e7b79afc23c9f878b526d454601f
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-06-13 16:59:24 +0300

    add package part

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -2,23 +2,58 @@
 
 set -euo pipefail
 
+source .buildkite/scripts/common.sh
+
 VERSION=$(awk '/const DefaultVersion/{print $NF}' version/version.go | tr -d '"')
 WORKSPACE="$(pwd)"
 PATH="${PATH}:${WORKSPACE}/bin"
 HOME="${WORKSPACE}"
-
-#setEnvVar('IS_BRANCH_AVAILABLE', isBranchUnifiedReleaseAvailable(env.BRANCH_NAME))
+IS_BRANCH_AVAILABLE=${BUILDKITE_BRANCH}
 PLATFORM_TYPE=$(uname -m)
-PLATFORMS=""
-PACKAGES=""
-if [[ ${PLATFORM_TYPE} == "arm" || ${PLATFORM_TYPE} == "aarch64" ]]; then
-    PLATFORMS="linux/arm64"
-    PACKAGES="docker"
+MATRIX_PLATFORM="$1"
+MATRIX_TYPE="$2"
+
+if [[ ${BUILDKITE_BRANCH} == "main" && ${MATRIX_TYPE} == "staging" ]]; then
+    echo "INFO: staging artifacts for the main branch are not required."
+else
+    PLATFORMS=""
+    PACKAGES=""
+    if [[ ${PLATFORM_TYPE} == "arm" || ${PLATFORM_TYPE} == "aarch64" ]]; then
+        PLATFORMS="linux/arm64"
+        PACKAGES="docker"
+    fi
+
+    if [[ ${MATRIX_TYPE} == "staging" ]]; then
+        MAKEGOAL="release-manager-release"
+    else
+        MAKEGOAL="release-manager-snapshot"
+    fi
+
+    cd ${BASE_DIR}
+    with_go
+
+    install_packages=(
+            "github.com/magefile/mage",
+            "github.com/elastic/go-licenser",
+            "golang.org/x/tools/cmd/goimports",
+            "github.com/jstemmer/go-junit-report",
+            "gotest.tools/gotestsum"
+    )
+
+    for pckg in "${install_packages}"; do
+    go install ${pckg}@latest
+    done
+
+    make ${MAKEGOAL}
 fi
 
-echo "Platform: '$1'"
-echo "Type: '$2'"
-
-echo ${VERSION}
-echo ${PLATFORM_TYPE}
-echo $(uname)
+if [[ ${BUILDKITE_BRANCH} == "main" && ${MATRIX_TYPE} == "staging" ]]; then
+    echo "INFO: staging artifacts for the main branch are not required."
+else
+    if [[ ${MATRIX_TYPE} == "staging" ]]; then
+        MAKEGOAL="release-manager-release"
+    else
+        MAKEGOAL="release-manager-snapshot"
+    fi
+    echo "uploading artifacts..."
+fi
