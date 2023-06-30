3df0044a7b0173aeaef8d31782103085e35d4dab
sharbuz <87968844+sharbuz@users.noreply.github.com>
2023-06-30 08:09:18 +0300

    Dra snapshot stage (#2702)
    * test the trigger step
    
    * test condition for trigger step
    
    * revert test condition for trigger step and add comment with TODO step for the future
    
    * fix condition
    
    * matrix test
    
    * matrix test2
    
    * matrix test3
    
    * fix the version path
    
    * platform and matrix test
    
    * platform and matrix test
    
    * platform and matrix test
    
    * platform and matrix test
    
    * update script permissions
    
    * fix values
    
    * fix platform type and add debug info
    
    * add package part
    
    * fix package part add publish part
    
    * add add_bin_path
    
    * add variable SETUP_GVM_VERSION
    
    * fix go module names
    
    * fix for loop for install packages
    
    * add ubuntu images
    
    * fix step keys
    
    * fix images
    
    * delete provider setting
    
    * change the image options
    
    * change the image options
    
    * change the image options
    
    * change gvm version
    
    * try to debug make error
    
    * add debur info into the package script
    
    * troubleshoot the script
    
    * troubleshoot the script
    
    * troubleshoot the script
    
    * troubleshoot the image
    
    * troubleshoot the image
    
    * revert the image
    
    * change the gcp image
    
    * change the gcp image
    
    * change the gcp image
    
    * add GO_VERSION
    
    * add debug to the comman.bash script
    
    * add debug to the comman.bash script
    
    * debug comman.bash script
    
    * debug comman.bash script
    
    * debug comman.bash script
    
    * debug comman.bash script
    
    * debug comman.bash script
    
    * revert common.bash and run_with_go_ver
    
    * revert common.bash and run_with_go_ver
    
    * change image
    
    * correct path
    
    * add google_cloud_auth
    
    * test
    
    * init commit for dra-snapshot stage
    
    * delete dependencies
    
    * add debug for rda_snapshot script
    
    * debug IS_BRANCH_AVAILABLE func script
    
    * debug IS_BRANCH_AVAILABLE func
    
    * debug IS_BRANCH_AVAILABLE func
    
    * debug IS_BRANCH_AVAILABLE func
    
    * debug IS_BRANCH_AVAILABLE func
    
    * debug IS_BRANCH_AVAILABLE func
    
    * test
    
    * delete condition
    
    * test
    
    * test
    
    * delete post-checkout changes
    
    * revert changes
    
    * add dra-snapshot stage
    
    * add pre-command
    
    * remove download
    
    * add donload and add_bin_path funcs
    
    * add previous steps and change agen's configs
    
    * add depends_on
    
    * change dependencies
    
    * change dependencies
    
    * fix destination add reports and pre=exit
    
    * add mkdir to download func
    
    * fix get_bucket_uri func
    
    * fix get_bucket_uri func
    
    * add condition for the dra-snapshot stage
    
    * add condition for the dra-snapshot stage
    
    * check condition for the dra-snapshot stage
    
    * refactored the check_if_file_exist_in_repo func
    
    * refactored the check_if_file_exist_in_repo func
    
    * refactored the check_if_file_exist_in_repo func
    
    * refactored the check_if_file_exist_in_repo func
    
    * check the check_if_file_exist_in_repo func add github auth
    
    * debug the check_if_file_exist_in_repo func
    
    * debug the check_if_file_exist_in_repo func
    
    * debug the check_if_file_exist_in_repo func
    
    * test the check_if_file_exist_in_repo func
    
    * test the check_if_file_exist_in_repo func with wrong file
    
    * test the check_if_file_exist_in_repo func with correct filename
    
    * add slack notifications for both pipelines
    
    * fixenvironment variables
    
    * add release-manager script change the getting the bucket name
    
    * refactor common.sh script
    
    * Update .buildkite/hooks/pre-command
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * Update .buildkite/hooks/pre-command
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * fix the cleanup func
    
    * fix the FOLDER variable
    
    * fix remarks
    
    * test FILE_EXISTS_IN_REPO=false
    
    * revert FILE_EXISTS_IN_REPO
    
    * add the default value of the branchName variable
    
    * change the cleanup func
    
    * add annotation for dra_snapshot.sh
    
    ---------
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>

---
+++ .buildkite/scripts/release-manager.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# This script is executed by the DRA stage.
+# It requires the below environment variables:
+# - BRANCH, the project branch
+# - PROJECT, the release manager project
+# - TYPE, the type of release (snapshot or staging)
+# - VERSION, the version (either a release or a snapshot)
+# - FOLDER, the relative folder where the binaries are stored
+# - OUTPUT_FILE, the file where the logs are stored.
+# - VAULT_ADDR
+# - VAULT_ROLE_ID
+# - VAULT_SECRET_ID
+#
+# It uses env variables to help to run this script with a simpler jenkins
+# pipeline call.
+#
+set -uexo pipefail
+
+readonly TYPE=${TYPE:-snapshot}
+readonly OUTPUT_FILE=${OUTPUT_FILE:-release-manager-report.out}
+
+# shellcheck source=/dev/null
+source /usr/local/bin/bash_standard_lib.sh
+
+# set required permissions on artifacts and directory
+chmod -R a+r "$FOLDER"/*
+chmod -R a+w "$FOLDER"
+
+# ensure the latest image has been pulled
+IMAGE=docker.elastic.co/infra/release-manager:latest
+(retry 3 docker pull --quiet "${IMAGE}") || echo "Error pulling ${IMAGE} Docker image, we continue"
+docker images --filter=reference=${IMAGE}
+
+# Generate checksum files and upload to GCS
+
+run_release_manager() {
+    echo "+++ Generate checksum files and upload to GCS..."
+    local dry_run="--dry-run"
+#    if [ "$BUILDKITE_PULL_REQUEST" != "false" ]; then
+#        dry_run="--dry-run"
+#    fi
+    docker run --rm \
+    --name release-manager \
+    -e VAULT_ADDR \
+    -e VAULT_ROLE_ID \
+    -e VAULT_SECRET_ID \
+    --mount type=bind,readonly=false,src="$PWD",target=/artifacts \
+    "$IMAGE" \
+      cli collect \
+        --project "${PROJECT}" \
+        --branch "${BRANCH}" \
+        --commit "$(git rev-parse HEAD)" \
+        --workflow "${TYPE}" \
+        --artifact-set main \
+        --version "${VERSION}" \
+        $dry_run 2>&1 | tee "$OUTPUT_FILE" \
+        #
+}
+
+run_release_manager
+
+RM_EXIT_CODE=$?
+
+exit $RM_EXIT_CODE
