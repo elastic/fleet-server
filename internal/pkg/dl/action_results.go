e1d2eb1a07a0076bc7826f731477a188d429e546
Julia Bardi <julia.bardi@elastic.co>
2023-07-13 09:11:07 +0200

    using actionID+agentID as action results doc id

--- internal/pkg/dl/action_results.go
+++ internal/pkg/dl/action_results.go
@@ -7,24 +7,30 @@ package dl
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"time"
 
 	"github.com/elastic/fleet-server/v7/internal/pkg/bulk"
+	"github.com/elastic/fleet-server/v7/internal/pkg/es"
 	"github.com/elastic/fleet-server/v7/internal/pkg/model"
 )
 
-func CreateActionResult(ctx context.Context, bulker bulk.Bulk, acr model.ActionResult) (string, error) {
+func CreateActionResult(ctx context.Context, bulker bulk.Bulk, acr model.ActionResult) error {
 	return createActionResult(ctx, bulker, FleetActionsResults, acr)
 }
 
-func createActionResult(ctx context.Context, bulker bulk.Bulk, index string, acr model.ActionResult) (string, error) {
+func createActionResult(ctx context.Context, bulker bulk.Bulk, index string, acr model.ActionResult) error {
 	if acr.Timestamp == "" {
 		acr.Timestamp = time.Now().UTC().Format(time.RFC3339)
 	}
 	body, err := json.Marshal(acr)
 	if err != nil {
-		return "", err
+		return err
 	}
 
-	return bulker.Create(ctx, index, acr.Id, body, bulk.WithRefresh())
+	_, err = bulker.Create(ctx, index, acr.ActionID+":"+acr.AgentID, body, bulk.WithRefresh())
+	if errors.Is(err, es.ErrElasticVersionConflict) {
+		return nil
+	}
+	return err
 }
