31576e4b4699ab3e3c180e06c147fc19ada1709d
Julien Lind <julien.lind@elastic.co>
2023-07-28 09:18:48 +0200

    Update release-manager.sh (#2847)
    Vault secret were not discoverable anymore due to the fact that we changed them to be redacted.
    Co-authored-by: Mario Rodriguez Molins <mario.rodriguez@elastic.co>

--- .buildkite/scripts/release-manager.sh
+++ .buildkite/scripts/release-manager.sh
@@ -43,9 +43,9 @@ run_release_manager() {
     fi
     docker run --rm \
     --name release-manager \
-    -e VAULT_ADDR_SECRET \
-    -e VAULT_ROLE_ID_SECRET \
-    -e VAULT_SECRET_ID_SECRET \
+    -e VAULT_ADDR="${VAULT_ADDR_SECRET}" \
+    -e VAULT_ROLE_ID="${VAULT_ROLE_ID_SECRET}" \
+    -e VAULT_SECRET_ID="${VAULT_SECRET_ID_SECRET}" \
     --mount type=bind,readonly=false,src="$PWD",target=/artifacts \
     "$IMAGE" \
       cli collect \
