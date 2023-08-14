5bc821f511e261fe391ab43fb58c6727f34e7af9
Craig MacKenzie <craig.mackenzie@elastic.co>
2023-08-14 14:02:10 -0400

    Restore log line deleted in merge.

--- internal/pkg/bulk/bulk_test.go
+++ internal/pkg/bulk/bulk_test.go
@@ -252,6 +252,8 @@ func TestCancelCtx(t *testing.T) {
 		},
 	}
 
+	log.Logger = testlog.SetLogger(t)
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ctx, cancelF := context.WithCancel(context.Background())
