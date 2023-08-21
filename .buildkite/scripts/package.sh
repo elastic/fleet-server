7562c807bb0ba473445297c7a745d49c319eb754
sharbuz <87968844+sharbuz@users.noreply.github.com>
2023-08-21 11:34:37 +0300

    backport the buildkite pipelines from the main branch to the branch 7.17 (#2891)
    * backport the buildkite pipelines from the main branch to the branch 7.17

---
+++ .buildkite/scripts/package.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common.sh
+
+VERSION="$(make get-version)"
+PLATFORM_TYPE=$(uname -m)
+TYPE="$1"
+INFRA_REPO="https://github.com/repos/elastic/infra/contents"
+
+if [[ ${BUILDKITE_BRANCH} == "main" && ${TYPE} == "staging" ]]; then
+    echo "INFO: staging artifacts for the main branch are not required."
+    exit 0
+fi
+
+PLATFORMS=""
+PACKAGES=""
+if [[ ${PLATFORM_TYPE} == "arm" || ${PLATFORM_TYPE} == "aarch64" ]]; then
+    PLATFORMS="linux/arm64"
+    PACKAGES="docker"
+fi
+
+add_bin_path
+with_go
+with_mage
+
+case "${TYPE}" in
+    "snapshot")
+        export SNAPSHOT=true
+        make release
+        ;;
+    "staging")
+        make release
+        ;;
+    *)
+    echo "The option is unsupported yet"
+    ;;
+esac
+
+google_cloud_auth
+upload_mbp_packages_to_gcp_bucket "build/distributions/**/*" "${TYPE}"
