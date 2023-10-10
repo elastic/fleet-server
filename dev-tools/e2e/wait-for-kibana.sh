4b5b634540008580145125140de5ac128aa09907
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2023-10-10 11:34:30 -0400

    [Fleet] Fix kibana config in e2e tests

--- dev-tools/e2e/wait-for-kibana.sh
+++ dev-tools/e2e/wait-for-kibana.sh
@@ -6,8 +6,16 @@ host="$1"
 shift
 cmd="$@"
 
+REPO_ROOT=$(cd $(dirname $(readlink -f "$0"))/../.. && pwd)
+
+
 
 until $(curl --output /dev/null --silent --head --fail "${host}/api/status"); do
+    if [[ ! $(docker compose  -f $REPO_ROOT/dev-tools/e2e/docker-compose.yml --env-file $REPO_ROOT/dev-tools/integration/.env ps  --filter status=running -q  kibana 2>/dev/null) ]]; then
+        echo "Kibana container is not running"
+        docker-compose -f $REPO_ROOT/dev-tools/e2e/docker-compose.yml --env-file $REPO_ROOT/dev-tools/integration/.env logs kibana
+        exit 1
+    fi
     printf '.'
     sleep 1
 done
