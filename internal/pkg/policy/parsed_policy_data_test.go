f2f5b1fbb60bf1bdeafc11cd0937a7f37a00b2d1
michel-laterman <michel.laterman@elastic.co>
2023-10-13 10:46:32 -0700

    Update go version to 1.21.3

--- internal/pkg/policy/parsed_policy_data_test.go
+++ internal/pkg/policy/parsed_policy_data_test.go
@@ -2,6 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
+//nolint:unused // linter is not has issues detecting these even though they are used in unit tests
 package policy
 
 const testPolicy = `
