b53db72c448fe4870c2c7e1ac4309f4fae0d83b3
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2023-10-24 13:10:39 -0400

    [Fleet] Improve error handling for invalid base64 token

--- internal/pkg/apikey/apikey_test.go
+++ internal/pkg/apikey/apikey_test.go
@@ -21,3 +21,42 @@ func TestMonitorLeadership(t *testing.T) {
 	assert.Equal(t, *apiKey, APIKey{" foo", "bar"})
 	assert.Equal(t, token, apiKey.Token())
 }
+
+func TestNewAPIKeyFromToken(t *testing.T) {
+	tests := []struct {
+		name        string
+		apiKey      string
+		expectError error
+	}{{
+		name:        "Invalid base64",
+		apiKey:      "invalidbase64",
+		expectError: ErrInvalidToken,
+	},
+		{
+			name:        "malformed token",
+			apiKey:      "dGVzdA==",
+			expectError: ErrMalformedToken,
+		},
+		{
+			name:        "Invalid utf8",
+			apiKey:      "dGVzdMlA",
+			expectError: ErrInvalidToken,
+		},
+		{
+			name:        "Valid api key",
+			apiKey:      "bURmODBZb0JrTU82QzJJaVVET1A6bmVRUnBsWEJRbmVTVFIwV3FtaVVFZw==",
+			expectError: nil,
+		}}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := NewAPIKeyFromToken(tc.apiKey)
+			if tc.expectError != nil {
+				assert.Error(t, err)
+				assert.ErrorIs(t, err, tc.expectError)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
