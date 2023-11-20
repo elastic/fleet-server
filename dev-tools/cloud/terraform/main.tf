531764b371f7d67c1bde46a5e8651968e01cb25d
Alexandros, Sapranidis <alexandros@elastic.co>
2023-11-20 15:53:42 +0200

    Add tags to the provisioned EC deployment
    This commit adds some tags to the provisioned elastic cloud deployment
    so that we can trace easily which deployment was created from which BK
    build job.
    
    Signed-off-by: Alexandros, Sapranidis <alexandros@elastic.co>

--- dev-tools/cloud/terraform/main.tf
+++ dev-tools/cloud/terraform/main.tf
@@ -25,6 +25,18 @@ locals {
 resource "random_uuid" "name" {
 }
 
+variable "pull_request" {
+  type=string
+  default=""
+  description="The github pull request number"
+}
+
+variable "buildkite_id" {
+  type=string
+  default=""
+  description="The buildkite build id associated with this deployment"
+}
+
 resource "ec_deployment" "deployment" {
   name                   = format("fleet server PR %s", random_uuid.name.result)
   region                 = "gcp-us-west2"
@@ -34,7 +46,10 @@ resource "ec_deployment" "deployment" {
   tags = {
     "created_with_terraform" = "true"
     "docker_image_ea"        = local.docker_image_ea
-  }
+    "provisioner" = "terraform"
+    "pull_request" = var.pull_request
+    "buildkite_id" = var.buildkite_id
+}
 
   elasticsearch {}
 
