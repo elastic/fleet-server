2e8253d71d3f3177cd5016d71de887d47be084a4
Alexandros Sapranidis <alexandros@elastic.co>
2023-12-06 16:52:56 +0200

    Revert "Reset Benchmark timer for Bulk"
    This reverts commit 69a5cf7e9dad470cd74a3ddf7f350d6e22525ad5.

--- internal/pkg/bulk/bulk_test.go
+++ internal/pkg/bulk/bulk_test.go
@@ -339,8 +339,6 @@ func benchmarkMockBulk(b *testing.B, samples [][]byte) {
 		go func(sampleData []byte) {
 			defer wait.Done()
 
-			b.ResetTimer()
-			b.StartTimer()
 			for j := 0; j < b.N; j++ {
 				// Create
 				id, err := bulker.Create(ctx, index, "", sampleData)
@@ -366,7 +364,6 @@ func benchmarkMockBulk(b *testing.B, samples [][]byte) {
 					b.Error(err)
 				}
 			}
-			b.StopTimer()
 		}(samples[i])
 	}
 
@@ -376,7 +373,7 @@ func benchmarkMockBulk(b *testing.B, samples [][]byte) {
 }
 
 func BenchmarkMockBulk(b *testing.B) {
-	b.StopTimer()
+
 	benchmarks := []int{1, 8, 64, 4096, 32768}
 
 	// Create the samples outside the loop to avoid accounting
