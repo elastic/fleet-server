41ec3c84c8b7ea7b4d0d3af5607068d64fcce2bd
Alexandros Sapranidis <alexandros@elastic.co>
2023-12-06 12:51:53 +0200

    Make annotation reflect status
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/run_benchmark.sh
+++ .buildkite/scripts/run_benchmark.sh
@@ -16,7 +16,7 @@ if [[ ${TYPE} == "pr" ]]; then
     BENCH_BASE=next.out make benchmark
     BENCH=$(cat build/next.out)
     buildkite-agent annotate --style 'success' --context "gobench_pr" --append << _EOF_
-### Benchmark for pull request
+#### Benchmark for pull request
 <details><summary>go bench output</summary>
 
 \`\`\`bash
@@ -34,7 +34,7 @@ if [[ ${TYPE} == "base" ]]; then
     BENCH_BASE=base.out make benchmark
     BENCH=$(cat build/base.out)
     buildkite-agent annotate --style 'success' --context "gobench_base" --append << _EOF_
-### Benchmark for the ${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
+#### Benchmark for the ${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
 <details><summary>go bench output for ${BUILDKITE_PULL_REQUEST_BASE_BRANCH}</summary>
 
 \`\`\`bash
@@ -55,17 +55,30 @@ if [[ ${TYPE} == "compare" ]]; then
     cat build/base.out| gobenchdata --json build/base.json
     cat build/next.out| gobenchdata --json build/next.json
     gobenchdata checks eval build/base.json build/next.json --json build/full_report.json
-    cat build/full_report.json| jq 'del(.Checks."My Check".Diffs[]| select(.Status == "pass") )'| tee build/failed_report.json
-    gobenchdata checks report failed.json | tee build/failed_summary.md
-    BENCH_COMPARE=$(cat build/failed_summary.md)
-    buildkite-agent annotate --style 'success' --context "benchstat" --append << _EOF_
-### Benchmark comparison
+    status=$(jq -r '.Status' build/full_report.json)
+    if [[ $status == "fail" ]]; then
+      cat build/full_report.json| jq 'del(.Checks."My Check".Diffs[]| select(.Status == "pass") )'| tee build/failed_report.json
+      gobenchdata checks report failed.json | tee build/failed_summary.md
+      BENCH_COMPARE=$(cat build/failed_summary.md)
+      buildkite-agent annotate --style 'error' --context "benchstat" --append << _EOF_
+#### Benchmark comparison
 <details><summary>Comparison table of benchmark results of HEAD compared to ${BUILDKITE_PULL_REQUEST_BASE_BRANCH}</summary>
 
 ${BENCH_COMPARE}
 
 </details>
 _EOF_
+    else
+      BENCH_COMPARE=$(gobenchdata checks report build/full_report.json)
+      buildkite-agent annotate --style 'success' --context "benchstat" --append << _EOF_
+#### Benchmark comparison
+<details><summary>No significant performance issue detect against ${BUILDKITE_PULL_REQUEST_BASE_BRANCH}</summary>
+
+${BENCH_COMPARE}
+
+</details>
+_EOF_
+    fi
 fi
 
 
