0ce131f4447160bd361aaa7192d58d4ffa8318d2
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-01-30 17:11:51 +0200

    test

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -105,35 +105,25 @@ google_cloud_auth() {
 upload_packages_to_gcp_bucket() {
     local pattern=${1}
     local baseUri="gs://${JOB_GCS_INGEST_BUCKET}/${REPO}"
-    local oldUri="gs://${JOB_GCS_BUCKET}/${REPO}"           #TODO remove after tests
-    local bucketUriCommit="${baseUri}"/commits/${BUILDKITE_COMMIT}
-    local oldBucketUriCommit="${oldUri}"/commits/${BUILDKITE_COMMIT}          #TODO remove after tests
-    local bucketUriDefault="${baseUri}"/snapshots
-    local oldBucketUriDefault="${oldUri}"/snapshots         #TODO remove after tests
+    local bucketUriCommit="${baseUri}/commits/${BUILDKITE_COMMIT}"
+    local bucketUriDefault="${baseUri}/snapshots"
 
     if [[ ${BUILDKITE_PULL_REQUEST} != "false" ]]; then
-        bucketUriDefault="${baseUri}"/pull-requests/pr-${GITHUB_PR_NUMBER}
-        oldBucketUriDefault="${oldUri}"/pull-requests/pr-${GITHUB_PR_NUMBER}         #TODO remove after tests
+        bucketUriDefault="${baseUri}/pull-requests/pr-${GITHUB_PR_NUMBER}"
     fi
     for bucketUri in "${bucketUriCommit}" "${bucketUriDefault}"; do
-        gsutil -m -q cp -a public-read -r ${pattern} "${bucketUri}"
-    done
-    #TODO remove the cycle below after tests
-    for oldBucketUri in "${oldBucketUriCommit}" "${oldBucketUriDefault}"; do
-        gsutil -m -q cp -a public-read -r ${pattern} "${oldBucketUri}"
+        gsutil -m cp -a public-read -r ${pattern} "${bucketUri}"          #TODO add "-q" option after tests
     done
 }
 
 get_bucket_uri() {
     local type=${1}
     local baseUri="gs://${JOB_GCS_INGEST_BUCKET}/jobs"
-    local oldUri="gs://${JOB_GCS_BUCKET}/jobs"         #TODO remove after tests
     if [[ ${type} == "snapshot" ]]; then
         local folder="commits"
     else
         local folder="${type}"
     fi
-    oldBucketUri="${oldUri}/${folder}/${BUILDKITE_COMMIT}"         #TODO remove after tests
     bucketUri="${baseUri}/${folder}/${BUILDKITE_COMMIT}"
 }
 
@@ -142,7 +132,6 @@ upload_mbp_packages_to_gcp_bucket() {
     local type=${2}
     get_bucket_uri "${type}"
     gsutil -m -q cp -a public-read -r ${pattern} ${bucketUri}
-    gsutil -m -q cp -a public-read -r ${pattern} ${oldBucketUri}        #TODO remove after tests
 }
 
 download_mbp_packages_from_gcp_bucket() {
