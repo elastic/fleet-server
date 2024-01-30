e607b503321c33004163eb042e70b638d3054e14
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-01-30 23:21:57 +0100

    Check FLEET_SERVER_URL and add annotations

--- dev-tools/cloud/launch_cloud_e2e.sh
+++ dev-tools/cloud/launch_cloud_e2e.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cleanup() {
   r=$?
 
+  echo "--- Cleaning deployment"
   make -C ${CLOUD_TESTING_BASE} cloud-clean
 
   exit $r
@@ -12,11 +13,24 @@ cleanup() {
 trap cleanup EXIT INT TERM
 
 
+echo "--- Creating deployment"
 make -C ${CLOUD_TESTING_BASE} cloud-deploy
 
 # Fleet server URL is obtained in "test-cloude2e-set" target
-# FLEET_SERVER_URL=$(make --no-print-directory -C ${CLOUD_TESTING_BASE} cloud-get-fleet-url)
+FLEET_SERVER_URL=$(make --no-print-directory -C ${CLOUD_TESTING_BASE} cloud-get-fleet-url)
 # export FLEET_SERVER_URL
+if [[ "${FLEET_SERVER_URL}" == "" ]]; then
+    message="FLEET_SERVER_URL is empty, cloud e2e tests cannot be executed"
+    if [[ "${CI}" == "true" ]]; then
+        buildkite-agent annotate \
+            "${message}" \
+            --context "ctx-cloude2e-test" \
+            --stype "warning"
+    fi
+    echo "${message}"
+    exit 0
+fi
 
+echo "--- Trigger cloud E2E test"
 make test-cloude2e-set | tee build/test-cloude2e-set
 
