f9dd655ef381de06fedfdb2e1dfa35152ee217b3
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-01-31 00:16:39 +0100

    Debug fleet server url value

--- dev-tools/cloud/launch_cloud_e2e.sh
+++ dev-tools/cloud/launch_cloud_e2e.sh
@@ -18,6 +18,7 @@ make -C ${CLOUD_TESTING_BASE} cloud-deploy
 
 # Fleet server URL is obtained in "test-cloude2e-set" target
 FLEET_SERVER_URL=$(make --no-print-directory -C ${CLOUD_TESTING_BASE} cloud-get-fleet-url)
+echo "Fleet server: \"${FLEET_SERVER_URL}\""
 # export FLEET_SERVER_URL
 if [[ "${FLEET_SERVER_URL}" == "" ]]; then
     message="FLEET_SERVER_URL is empty, cloud e2e tests cannot be executed"
