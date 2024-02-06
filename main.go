9e3368ae582461e3ccbc5f0addbaad5c9c66b347
elasticmachine <infra-root+elasticmachine@elastic.co>
2024-02-06 10:18:29 +0000

    [Release] update version

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/elastic/fleet-server/v7/internal/pkg/build"
 )
 
-const defaultVersion = "7.17.18"
+const defaultVersion = "7.17.19"
 
 var (
 	Version   string = defaultVersion
