2962db6f8b3047a562093d22faf56fe08a8fc2e5
Julia Bardi <julia.bardi@elastic.co>
2024-02-15 09:58:45 +0100

    remove duplicated code

--- internal/pkg/apikey/auth_test.go
+++ internal/pkg/apikey/auth_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/elastic/fleet-server/v7/internal/pkg/testing/esutil"
+	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,30 +19,28 @@ const (
 	rawToken = " foo:bar"
 )
 
-func TestAuth429(t *testing.T) {
+func setup(t *testing.T, statusCode int) (context.Context, *APIKey, *elasticsearch.Client) {
 	token := base64.StdEncoding.EncodeToString([]byte(rawToken))
 	apiKey, err := NewAPIKeyFromToken(token)
 	assert.NoError(t, err)
 	ctx := context.Background()
 
 	mockES, mockTransport := esutil.MockESClient(t)
-	mockTransport.RoundTripFn = func(req *http.Request) (*http.Response, error) { return &http.Response{StatusCode: 429}, nil }
+	mockTransport.RoundTripFn = func(req *http.Request) (*http.Response, error) { return &http.Response{StatusCode: statusCode}, nil }
 
-	_, err = apiKey.Authenticate(ctx, mockES)
+	return ctx, apiKey, mockES
+}
+
+func TestAuth429(t *testing.T) {
+	ctx, apiKey, mockES := setup(t, 429)
+	_, err := apiKey.Authenticate(ctx, mockES)
 
 	assert.Equal(t, "elasticsearch auth limit: apikey auth response  foo: [429 Too Many Requests] ", err.Error())
 }
 
 func TestAuth401(t *testing.T) {
-	token := base64.StdEncoding.EncodeToString([]byte(rawToken))
-	apiKey, err := NewAPIKeyFromToken(token)
-	assert.NoError(t, err)
-	ctx := context.Background()
-
-	mockES, mockTransport := esutil.MockESClient(t)
-	mockTransport.RoundTripFn = func(req *http.Request) (*http.Response, error) { return &http.Response{StatusCode: 401}, nil }
-
-	_, err = apiKey.Authenticate(ctx, mockES)
+	ctx, apiKey, mockES := setup(t, 401)
+	_, err := apiKey.Authenticate(ctx, mockES)
 
 	assert.Equal(t, "unauthorized: apikey auth response  foo: [401 Unauthorized] ", err.Error())
 }
