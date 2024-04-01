d0e3e271df9fed5bcf97e62106210aa9d6662196
Kaan Yalti <kaan.yalti@elastic.co>
2024-04-01 14:08:51 -0400

    Update internal/pkg/api/handleUpload.go
    Co-authored-by: Michel Laterman <82832767+michel-laterman@users.noreply.github.com>

--- internal/pkg/api/handleUpload.go
+++ internal/pkg/api/handleUpload.go
@@ -185,7 +185,7 @@ func (ut *UploadT) validateUploadCompleteRequest(r *http.Request, id string) (st
 
 	var req UploadCompleteRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		return "", &BadRequestErr{msg: "unable to parse upload complete request body"}
+		&BadRequestErr{msg: "unable to decode upload complete request"}
 	}
 
 	hash := strings.TrimSpace(req.Transithash.Sha256)
