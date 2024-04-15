0e70530850e80f08c305385b031c8c699921154f
michel-laterman <michel.laterman@elastic.co>
2024-04-15 08:12:21 -0700

    Add delay and 2nd health check to int server startup

--- internal/pkg/server/fleet_integration_test.go
+++ internal/pkg/server/fleet_integration_test.go
@@ -296,6 +296,24 @@ func (s *tserver) waitServerUp(ctx context.Context, dur time.Duration) error {
 		case <-time.After(100 * time.Millisecond):
 		}
 	}
+
+	// small delay to allow config change to propogate
+	time.Sleep(100 * time.Millisecond)
+	for {
+		healthy, err := isHealthy()
+		if err != nil {
+			return err
+		}
+		if healthy {
+			break
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(100 * time.Millisecond):
+		}
+	}
 	return nil
 }
 
