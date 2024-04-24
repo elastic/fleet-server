9fcf4b28e5ef27a882cb564c1380de745dc4260f
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2024-04-24 08:28:44 -0700

    Revert "agent-mode loads output from policy (#3411)"
    This reverts commit fe7955b1c27150da81a6e1a38f86d03b641dc680.

--- internal/pkg/config/output.go
+++ internal/pkg/config/output.go
@@ -5,7 +5,6 @@
 package config
 
 import (
-	"crypto/tls"
 	"fmt"
 	"net"
 	"net/http"
@@ -25,14 +24,6 @@ import (
 // Giving it some sane long value.
 const httpTransportLongPollTimeout = 10 * time.Minute
 const schemeHTTP = "http"
-
-const (
-	DefaultElasticsearchHost             = "localhost:9200"
-	DefaultElasticsearchTimeout          = 90 * time.Second
-	DefaultElasticsearchMaxRetries       = 3
-	DefaultElasticsearchMaxConnPerHost   = 128
-	DefaultElasticsearchMaxContentLength = 100 * 1024 * 1024
-)
 
 var hasScheme = regexp.MustCompile(`^([a-z][a-z0-9+\-.]*)://`)
 
@@ -63,11 +54,11 @@ type Elasticsearch struct {
 // InitDefaults initializes the defaults for the configuration.
 func (c *Elasticsearch) InitDefaults() {
 	c.Protocol = schemeHTTP
-	c.Hosts = []string{DefaultElasticsearchHost}
-	c.Timeout = DefaultElasticsearchTimeout
-	c.MaxRetries = DefaultElasticsearchMaxRetries
-	c.MaxConnPerHost = DefaultElasticsearchMaxConnPerHost
-	c.MaxContentLength = DefaultElasticsearchMaxContentLength
+	c.Hosts = []string{"localhost:9200"}
+	c.Timeout = 90 * time.Second
+	c.MaxRetries = 3
+	c.MaxConnPerHost = 128
+	c.MaxContentLength = 100 * 1024 * 1024
 }
 
 // Validate ensures that the configuration is valid.
@@ -180,108 +171,6 @@ func (c *Elasticsearch) ToESConfig(longPoll bool) (elasticsearch.Config, error) 
 		MaxRetries:   c.MaxRetries,
 		DisableRetry: disableRetry,
 	}, nil
-}
-
-// MergeElasticsearchPolicy will merge elasticsearch settings retrieved from the fleet-server's policy into the base configuration and return the resulting config.
-// ucfg.Merge and config.Config.Merge will both fail at merging configs because the verification mode is not detect as a string type value
-func MergeElasticsearchFromPolicy(cfg, pol Elasticsearch) Elasticsearch {
-	res := Elasticsearch{
-		Protocol:         cfg.Protocol,
-		Hosts:            cfg.Hosts,
-		Headers:          cfg.Headers,
-		ServiceToken:     cfg.ServiceToken, // ServiceToken will always be specified from the settings and not in the policy.
-		ServiceTokenPath: cfg.ServiceTokenPath,
-		ProxyURL:         cfg.ProxyURL,
-		ProxyDisable:     cfg.ProxyDisable,
-		ProxyHeaders:     cfg.ProxyHeaders,
-		TLS:              mergeElasticsearchTLS(cfg.TLS, pol.TLS), // tls can be a special case
-		MaxRetries:       cfg.MaxRetries,
-		MaxConnPerHost:   cfg.MaxConnPerHost,
-		Timeout:          cfg.Timeout,
-		MaxContentLength: cfg.MaxContentLength,
-	}
-	// If policy has a non-default Hosts value use it's values for Protocol and hosts
-	if pol.Hosts != nil && !(len(pol.Hosts) == 1 && pol.Hosts[0] == DefaultElasticsearchHost) {
-		res.Protocol = pol.Protocol
-		res.Hosts = pol.Hosts
-	}
-	if pol.Headers != nil {
-		res.Headers = pol.Headers
-	}
-	// If the policy ProxyURL is set, use all of the policy's Proxy values.
-	if pol.ProxyURL != "" {
-		res.ProxyURL = pol.ProxyURL
-		res.ProxyDisable = pol.ProxyDisable
-		res.ProxyHeaders = pol.ProxyHeaders
-	}
-	if pol.MaxRetries != DefaultElasticsearchMaxRetries {
-		res.MaxRetries = pol.MaxRetries
-	}
-	if pol.MaxConnPerHost != DefaultElasticsearchMaxConnPerHost {
-		res.MaxConnPerHost = pol.MaxConnPerHost
-	}
-	if pol.Timeout != DefaultElasticsearchTimeout {
-		res.Timeout = pol.Timeout
-	}
-	if pol.MaxContentLength != DefaultElasticsearchMaxContentLength {
-		res.MaxContentLength = pol.MaxContentLength
-	}
-	return res
-}
-
-// mergeElasticsearchTLS merges the TLS settings received from the fleet-server's policy into the settings the agent passes
-func mergeElasticsearchTLS(cfg, pol *tlscommon.Config) *tlscommon.Config {
-	if cfg == nil && pol == nil {
-		return nil
-	} else if cfg == nil && pol != nil {
-		return pol
-	} else if cfg != nil && pol == nil {
-		return cfg
-	}
-	res := &tlscommon.Config{
-		Enabled:              cfg.Enabled,
-		VerificationMode:     cfg.VerificationMode,
-		Versions:             cfg.Versions,
-		CipherSuites:         cfg.CipherSuites,
-		CAs:                  cfg.CAs,
-		Certificate:          cfg.Certificate,
-		CurveTypes:           cfg.CurveTypes,
-		Renegotiation:        cfg.Renegotiation,
-		CASha256:             cfg.CASha256,
-		CATrustedFingerprint: cfg.CATrustedFingerprint,
-	}
-	if pol.Enabled != nil {
-		res.Enabled = pol.Enabled
-	}
-	if pol.VerificationMode != tlscommon.VerifyFull {
-		res.VerificationMode = pol.VerificationMode // VerificationMode defaults to VerifyFull
-	}
-	if pol.Versions != nil {
-		res.Versions = pol.Versions
-	}
-	if pol.CipherSuites != nil {
-		res.CipherSuites = pol.CipherSuites
-	}
-	if pol.CAs != nil {
-		res.CAs = pol.CAs
-	}
-	if pol.Certificate.Certificate != "" {
-		res.Certificate = pol.Certificate
-	}
-	if pol.CurveTypes != nil {
-		res.CurveTypes = pol.CurveTypes
-	}
-	if pol.Renegotiation != tlscommon.TLSRenegotiationSupport(tls.RenegotiateNever) {
-		res.Renegotiation = pol.Renegotiation
-	}
-	if pol.CASha256 != nil {
-		res.CASha256 = pol.CASha256
-	}
-	if pol.CATrustedFingerprint != "" {
-		res.CATrustedFingerprint = pol.CATrustedFingerprint
-	}
-
-	return res
 }
 
 // Validate validates that only elasticsearch is defined on the output.
