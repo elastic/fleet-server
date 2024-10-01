468246a3630293e7a72f2f5fbfb67f94378c9cb6
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2024-10-01 09:00:00 -0700

    Add secret_paths attributes to policies sent to agents (#3908)
    Add a new attribute "secret_paths" to the policy data sent to agents.
    This attribute is a list of keys where the fleet-server has repalced a
    reference with a secret value. The agent is expected to redact the
    values of these keys when outputting policy data.
    
    (cherry picked from commit 4864cf419c133ea2b0fda835c4c50b133180a4d8)

--- internal/pkg/server/fleet_secrets_integration_test.go
+++ internal/pkg/server/fleet_secrets_integration_test.go
@@ -17,11 +17,12 @@ import (
 
 	"testing"
 
-	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/gofrs/uuid"
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/go-elasticsearch/v8"
 
 	"github.com/elastic/fleet-server/v7/internal/pkg/api"
 	"github.com/elastic/fleet-server/v7/internal/pkg/apikey"
@@ -35,7 +36,7 @@ type SecretResponse struct {
 	ID string
 }
 
-func createSecret(t *testing.T, ctx context.Context, bulker bulk.Bulk) string {
+func createSecret(t *testing.T, ctx context.Context, bulker bulk.Bulk, value string) string {
 	t.Log("Setup secret for fleet integration test")
 	esClient := bulker.Client()
 
@@ -55,7 +56,7 @@ func createSecret(t *testing.T, ctx context.Context, bulker bulk.Bulk) string {
 	defer res.Body.Close()
 	require.Equal(t, http.StatusOK, res.StatusCode)
 
-	req, err = http.NewRequestWithContext(ctx, "POST", "/_fleet/secret/", bytes.NewBuffer([]byte("{\"value\":\"secret_value\"}")))
+	req, err = http.NewRequestWithContext(ctx, "POST", "/_fleet/secret/", strings.NewReader(fmt.Sprintf(`{"value":%q}`, value)))
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "application/json")
 	// kibana_test:changeme base64 encoded
@@ -81,12 +82,15 @@ func createSecret(t *testing.T, ctx context.Context, bulker bulk.Bulk) string {
 	return resp.ID
 }
 
-func createAgentPolicyWithSecrets(t *testing.T, ctx context.Context, bulker bulk.Bulk, secretID string, secretRef string) string {
+func createAgentPolicyWithSecrets(t *testing.T, ctx context.Context, bulker bulk.Bulk, secretID, secretRef, outputID string) string {
 	policyID := uuid.Must(uuid.NewV4()).String()
 	var policyData = model.PolicyData{
 		Outputs: map[string]map[string]interface{}{
 			"default": {
 				"type": "elasticsearch",
+				"secrets": map[string]interface{}{
+					"secret-key": map[string]interface{}{"id": outputID},
+				},
 			},
 		},
 		OutputPermissions: json.RawMessage(`{"default":{}}`),
@@ -161,12 +165,14 @@ func Test_Agent_Policy_Secrets(t *testing.T) {
 	require.NoError(t, err)
 	ctx = testlog.SetLogger(t).WithContext(ctx)
 
+	// create secret for use with output
+	outputSecretID := createSecret(t, ctx, srv.bulker, "output_secret_value")
 	// create secret with kibana_system user
-	secretID := createSecret(t, ctx, srv.bulker)
+	secretID := createSecret(t, ctx, srv.bulker, "secret_value")
 	secretRef := fmt.Sprintf("$co.elastic.secret{%s}", secretID)
 
 	// create agent policy with secret reference
-	enrollKey := createAgentPolicyWithSecrets(t, ctx, srv.bulker, secretID, secretRef)
+	enrollKey := createAgentPolicyWithSecrets(t, ctx, srv.bulker, secretID, secretRef, outputSecretID)
 	cli := cleanhttp.DefaultClient()
 	// enroll an agent
 	t.Log("Enroll an agent")
@@ -230,4 +236,25 @@ func Test_Agent_Policy_Secrets(t *testing.T) {
 		"package_var_secret": "secret_value",
 		"type":               "fleet-server",
 	}, input)
+
+	// expect output secret to be replaced
+	output := (*actionData.Policy.Outputs)["default"]
+	assert.Conditionf(t, func() bool {
+		mp, ok := output.(map[string]interface{})
+		if !ok {
+			return false
+		}
+		v, ok := mp["secret-key"]
+		if !ok {
+			return false
+		}
+		s, ok := v.(string)
+		if !ok {
+			return false
+		}
+		return s == "output_secret_value"
+	}, "expected output to contain secret-key: output_secret_value, got %v", output)
+	assert.NotContains(t, output, "secrets")
+	// expect that secret_paths lists the key
+	assert.ElementsMatch(t, []string{"inputs.0.package_var_secret", "outputs.default.secret-key"}, *actionData.Policy.SecretPaths)
 }
