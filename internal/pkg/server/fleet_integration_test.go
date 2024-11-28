6a7abcb944d673ef27449b12bdb7dd0f7c5a1f8b
michel-laterman <michel.laterman@elastic.co>
2024-11-28 12:38:36 -0800

    Fix race in test

--- internal/pkg/server/fleet_integration_test.go
+++ internal/pkg/server/fleet_integration_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/gofrs/uuid"
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/go-cleanhttp"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sync/errgroup"
@@ -206,7 +207,7 @@ func startTestServer(t *testing.T, ctx context.Context, policyD model.PolicyData
 		}
 	}
 
-	l := testlog.SetLogger(t)
+	l := zerolog.Nop()
 	srv, err := NewFleet(build.Info{Version: serverVersion}, state.NewLog(&l), false)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create server: %w", err)
