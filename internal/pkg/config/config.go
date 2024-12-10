5606d0ae90ca66a821560c8468d4a6c16442a5b0
michel-laterman <michel.laterman@elastic.co>
2024-12-10 09:38:01 -0800

    Fix linter

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -248,7 +248,6 @@ func (c *Config) Redact() *Config {
 }
 
 func checkDeprecatedOptions(deprecatedOpts map[string]string, c *ucfg.Config) {
-	keys := c.FlattenedKeys()
 	for opt, message := range deprecatedOpts {
 		if c.HasField(opt) {
 			zerolog.Ctx(context.TODO()).Warn().Msg(message) // TODO is used as this may be called before logger config is read.
