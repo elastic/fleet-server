951bca88ee2bd57390ceb2113f16de228309ca1f
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2025-02-06 10:48:32 -0800

    Use chi/middleware.Throttle instead of custom conn limiter (#4402)
    Use chi/middleware.Throttle to track in-flight requests and return a 429 response once the limit is reached.
    
    This is different behaviour from the TCP listener based approach where the check occurs before the TLS handshake and if the limit is reached, the connection is silently closed.
    
    (cherry picked from commit fbe3b2b6e9b38917c3a4003c832d5aaea6c6b1cf)
    
    # Conflicts:
    #	internal/pkg/api/server.go
    #	internal/pkg/limit/listener.go

--- internal/pkg/api/server.go
+++ internal/pkg/api/server.go
@@ -17,7 +17,6 @@ import (
 	"github.com/elastic/fleet-server/v7/internal/pkg/build"
 	"github.com/elastic/fleet-server/v7/internal/pkg/bulk"
 	"github.com/elastic/fleet-server/v7/internal/pkg/config"
-	"github.com/elastic/fleet-server/v7/internal/pkg/limit"
 	"github.com/elastic/fleet-server/v7/internal/pkg/logger"
 	"github.com/elastic/fleet-server/v7/internal/pkg/policy"
 	"go.elastic.co/apm/v2"
@@ -33,7 +32,7 @@ type server struct {
 
 // NewServer creates a new HTTP api for the passed addr.
 //
-// The server has a listener specific conn limit and endpoint specific rate-limits.
+// The server has an http request limit and endpoint specific rate-limits.
 // The underlying API structs (such as *CheckinT) may be shared between servers.
 func NewServer(addr string, cfg *config.Server, ct *CheckinT, et *EnrollerT, at *ArtifactT, ack *AckT, st *StatusT, sm policy.SelfMonitor, bi build.Info, ut *UploadT, ft *FileDeliveryT, pt *PGPRetrieverT, audit *AuditT, bulker bulk.Bulk, tracer *apm.Tracer) *server {
 	a := &apiServer{
@@ -89,13 +88,6 @@ func (s *server) Run(ctx context.Context) error {
 			zerolog.Ctx(ctx).Warn().Err(err).Msg("server.Run: error while closing listener.")
 		}
 	}()
-
-	// Conn Limiter must be before the TLS handshake in the stack;
-	// The server should not eat the cost of the handshake if there
-	// is no capacity to service the connection.
-	// Also, it appears the HTTP2 implementation depends on the tls.Listener
-	// being at the top of the stack.
-	ln = wrapConnLimitter(ctx, ln, s.cfg)
 
 	if s.cfg.TLS != nil && s.cfg.TLS.IsEnabled() {
 		commonTLSCfg, err := tlscommon.LoadTLSServerConfig(s.cfg.TLS)
@@ -163,6 +155,7 @@ func diagConn(c net.Conn, s http.ConnState) {
 	}
 }
 
+<<<<<<< HEAD
 func wrapConnLimitter(ctx context.Context, ln net.Listener, cfg *config.Server) net.Listener {
 	hardLimit := cfg.Limits.MaxConnections
 
@@ -179,6 +172,8 @@ func wrapConnLimitter(ctx context.Context, ln net.Listener, cfg *config.Server) 
 	return ln
 }
 
+=======
+>>>>>>> fbe3b2b (Use chi/middleware.Throttle instead of custom conn limiter (#4402))
 type stubLogger struct {
 	log zerolog.Logger
 }
