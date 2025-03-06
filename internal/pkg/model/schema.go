33acbf4557201ea7a05f7010f0cc91f634a46f5f
Jill Guyonnet <jill.guyonnet@gmail.com>
2025-03-06 19:12:23 +0100

    Run make generate

--- internal/pkg/model/schema.go
+++ internal/pkg/model/schema.go
@@ -216,6 +216,9 @@ type Agent struct {
 	// Date/time the Elastic Agent was last updated
 	UpdatedAt string `json:"updated_at,omitempty"`
 
+	// List of timestamps of attempts of Elastic Agent automatic upgrades
+	UpgradeAttempts []string `json:"upgrade_attempts,omitempty"`
+
 	// Additional upgrade status details.
 	UpgradeDetails *UpgradeDetails `json:"upgrade_details,omitempty"`
 
@@ -227,9 +230,6 @@ type Agent struct {
 
 	// Date/time the Elastic Agent was last upgraded
 	UpgradedAt string `json:"upgraded_at,omitempty"`
-
-	// List of timestamps of attempts of Elastic Agent automatic upgrades
-	UpgradeAttempts []string `json:"upgrade_attempts,omitempty"`
 
 	// User provided metadata information for the Elastic Agent
 	UserProvidedMetadata json.RawMessage `json:"user_provided_metadata,omitempty"`
