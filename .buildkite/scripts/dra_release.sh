d85b3443c42c84259d6016784c59549e79fab15f
michel-laterman <michel.laterman@elastic.co>
2025-03-12 09:31:31 -0700

    Add DRA FIPS artifacts to package pipeline

--- .buildkite/scripts/dra_release.sh
+++ .buildkite/scripts/dra_release.sh
@@ -41,6 +41,11 @@ if [[ ${TYPE} == "snapshot" ]]; then
     VERSION="${VERSION}-SNAPSHOT"
 fi
 
+if [[ ${FIPS} == "true" ]]; then
+    export FIPS=true
+    VERSION="${VERSION}-fips" # NOTE: fips artifact naming is fleet-server-fips-VERSION-OS-ARCH, not VERSION-fips
+fi
+
 mkdir -p ${BASE_DIR}/reports
 ./dev-tools/dependencies-report --csv ${BASE_DIR}/reports/dependencies-${VERSION}.csv
 cd ${BASE_DIR}/reports && shasum -a 512 dependencies-${VERSION}.csv > dependencies-${VERSION}.csv.sha512
