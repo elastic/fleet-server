8bb3c5e36951132edcf81253c4ca63feadcfe187
michel-laterman <michel.laterman@elastic.co>
2025-03-27 11:52:21 -0600

    Move FIPS to env

--- .buildkite/scripts/fips_provider_test.sh
+++ .buildkite/scripts/fips_provider_test.sh
@@ -8,6 +8,6 @@ add_bin_path
 
 with_msft_go
 
-echo "Starting the provider tests..."
-FIPS=true make test-unit junit-report
+echo "Starting the provider enabled tests..."
+make test-unit junit-report
 
