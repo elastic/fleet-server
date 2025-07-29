91bee0b38b8add6c7c5dc8a66b0b0f6a37b38236
michel-laterman <michel.laterman@elastic.co>
2025-07-29 08:42:34 -0700

    Fix linter

--- internal/pkg/dl/policies_integration_test.go
+++ internal/pkg/dl/policies_integration_test.go
@@ -64,7 +64,6 @@ func storeRandomPolicies(ctx context.Context, bulker bulk.Bulk, index string, co
 	return nil
 }
 
-//nolint:dupl // test duplication
 func TestQueryLatestPolicies(t *testing.T) {
 	ctx := testlog.SetLogger(t).WithContext(t.Context())
 
@@ -122,6 +121,8 @@ func TestQueryLatestPolicies400k(t *testing.T) {
 }
 
 // TesyQueryLatestPolices11kUnique tests behaviour when 11k unique policies are used, there is a 10k size specification in the aggregation.
+//
+//nolint:dupl // test duplication
 func TestQueryLatestPolicies11kUnique(t *testing.T) {
 	t.Skip("Re-enable after policy load issues have been sorted: https://github.com/elastic/fleet-server/issues/3254")
 	ctx := testlog.SetLogger(t).WithContext(t.Context())
