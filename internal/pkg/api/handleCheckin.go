78db76824ec1279c927cde1812ba22ecb1099136
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2025-07-29 16:19:55 -0700

    Fix download_rate as string on checkin handling (#5176)
    Fix fleet-server to properly handle when an agent sends the upgrade downloading detail's download_rate as a string instead of float64.
    
    (cherry picked from commit c94d8d9635f338445ed50eff6b4821d7f8eb2748)
    
    # Conflicts:
    #	testing/e2e/scaffold/scaffold.go

--- internal/pkg/api/handleCheckin.go
+++ internal/pkg/api/handleCheckin.go
@@ -453,10 +453,14 @@ func (ct *CheckinT) processUpgradeDetails(ctx context.Context, agent *model.Agen
 			vSpan.End()
 			break // no validation
 		}
-		_, err := details.Metadata.AsUpgradeMetadataDownloading()
+		upgradeDetails, err := details.Metadata.AsUpgradeMetadataDownloading()
 		if err != nil {
 			vSpan.End()
 			return fmt.Errorf("%w %s: %w", ErrInvalidUpgradeMetadata, UpgradeDetailsStateUPGDOWNLOADING, err)
+		}
+		if err := details.Metadata.FromUpgradeMetadataDownloading(upgradeDetails); err != nil {
+			vSpan.End()
+			return fmt.Errorf("%w %s: unable to repack metadata: %w", ErrInvalidUpgradeMetadata, UpgradeDetailsStateUPGDOWNLOADING, err)
 		}
 	case UpgradeDetailsStateUPGFAILED:
 		if details.Metadata == nil {
@@ -471,6 +475,11 @@ func (ct *CheckinT) processUpgradeDetails(ctx context.Context, agent *model.Agen
 		if meta.ErrorMsg == "" {
 			vSpan.End()
 			return fmt.Errorf("%w: %s metadata contains empty error_msg attribute", ErrInvalidUpgradeMetadata, UpgradeDetailsStateUPGFAILED)
+		}
+		// Repack metadata in failed case as the agent may send UPG_DOWNLOADING attributes.
+		if err = details.Metadata.FromUpgradeMetadataFailed(meta); err != nil {
+			vSpan.End()
+			return fmt.Errorf("%w %s: unable to repack metadata: %w", ErrInvalidUpgradeMetadata, UpgradeDetailsStateUPGFAILED, err)
 		}
 	case UpgradeDetailsStateUPGSCHEDULED:
 		if details.Metadata == nil {
