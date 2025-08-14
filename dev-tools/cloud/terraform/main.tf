397f0c6b48629ed59830e63f9be6a64b356d8243
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2025-08-13 18:38:28 -0700

    Use integration/.env ELASTICSEARCH_VERSION as stack version for cloude2e (#5252)
    (cherry picked from commit 46f80f85bd87ad68f50b6bb02078e098d4ea9029)
    
    # Conflicts:
    #	dev-tools/cloud/terraform/main.tf

--- dev-tools/cloud/terraform/main.tf
+++ dev-tools/cloud/terraform/main.tf
@@ -16,12 +16,35 @@ variable "elastic_agent_docker_image" {
   description = "Elastic agent docker image with tag."
 }
 
+<<<<<<< HEAD
+=======
+variable "git_commit" {
+  type        = string
+  default     = ""
+  description = "The git commit ID."
+}
+
+variable "pull_request" {
+  type        = string
+  default     = ""
+  description = "The github pull request number."
+}
+
+variable "ess_region" {
+  type        = string
+  default     = "gcp-us-west2"
+  description = "The ESS region to use"
+}
+
+>>>>>>> 46f80f8 (Use integration/.env ELASTICSEARCH_VERSION as stack version for cloude2e (#5252))
 locals {
-  match           = regex("const DefaultVersion = \"(.*)\"", file("${path.module}/../../../version/version.go"))[0]
-  stack_version   = format("%s-SNAPSHOT", local.match)
+  // strip hash found in ELASTICSEARCH_VERSION in integration/.env to get stack_version
+  dra_match       = regex("ELASTICSEARCH_VERSION=([0-9]+\\.[0-9]+\\.[0-9]+)(?:-[[:alpha:]]+-)?-?(SNAPSHOT)?", file("${path.module}/../../integration/.env"))
+  stack_version   = local.dra_match[1] == "SNAPSHOT" ? format("%s-SNAPSHOT", local.dra_match[0]) : local.dra_match[0]
   docker_image_ea = var.elastic_agent_docker_image
 }
 
+<<<<<<< HEAD
 resource "random_uuid" "name" {
 }
 
@@ -47,6 +70,17 @@ resource "ec_deployment" "deployment" {
   name                   = format("fleet server PR %s", random_uuid.name.result)
   region                 = "gcp-us-west2"
   version                = local.stack_version
+=======
+data "ec_stack" "latest" {
+  version_regex = local.stack_version
+  region        = var.ess_region
+}
+
+resource "ec_deployment" "deployment" {
+  name                   = format("fleet server PR-%s-%s", var.pull_request, var.git_commit)
+  region                 = var.ess_region
+  version                = data.ec_stack.latest.version
+>>>>>>> 46f80f8 (Use integration/.env ELASTICSEARCH_VERSION as stack version for cloude2e (#5252))
   deployment_template_id = "gcp-general-purpose"
 
   tags = {
