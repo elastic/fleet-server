deb2017a63c1f22db2ed4e66e170b126256154f5
Blake Rouse <blake.rouse@elastic.co>
2025-11-06 15:45:02 -0500

    Handle malformatted JSON in `.fleet-agents` `components` field (#5858)
    * Handle malformatted JSON in .fleet-agents components field.
    
    * Add changelog.
    
    * Add logs and update test to use require.
    
    (cherry picked from commit f9dbe416a6acc0eca64af40310496bd1eb84c9b1)
    
    # Conflicts:
    #	internal/pkg/api/handleCheckin.go

--- internal/pkg/api/handleCheckin.go
+++ internal/pkg/api/handleCheckin.go
@@ -959,10 +959,11 @@ func parseMeta(zlog zerolog.Logger, agent *model.Agent, req *CheckinRequest) ([]
 		return nil, nil
 	}
 
-	// Deserialize the agent's metadata copy
+	// Deserialize the agent's metadata copy. If it fails, it's ignored as it will just
+	// be replaced with the correct contents from the clients checkin.
 	var agentLocalMeta interface{}
 	if err := json.Unmarshal(agent.LocalMetadata, &agentLocalMeta); err != nil {
-		return nil, fmt.Errorf("parseMeta local: %w", err)
+		zlog.Warn().Err(err).Msg("local_metadata in document invalid; ignoring it")
 	}
 
 	var outMeta []byte
@@ -999,14 +1000,13 @@ func parseComponents(zlog zerolog.Logger, agent *model.Agent, req *CheckinReques
 		return nil, &unhealthyReason, nil
 	}
 
-	agentComponentsJSON, err := json.Marshal(agent.Components)
-	if err != nil {
-		return nil, &unhealthyReason, fmt.Errorf("agent.Components marshal: %w", err)
-	}
-
 	// Quick comparison first; compare the JSON payloads.
 	// If the data is not consistently normalized, this short-circuit will not work.
+<<<<<<< HEAD
 	if bytes.Equal(*req.Components, agentComponentsJSON) {
+=======
+	if bytes.Equal(req.Components, agent.Components) {
+>>>>>>> f9dbe41 (Handle malformatted JSON in `.fleet-agents` `components` field (#5858))
 		zlog.Trace().Msg("quick comparing agent components data is equal")
 		return nil, &unhealthyReason, nil
 	}
@@ -1024,14 +1024,26 @@ func parseComponents(zlog zerolog.Logger, agent *model.Agent, req *CheckinReques
 		return nil, &unhealthyReason, nil
 	}
 
+	// Deserialize the agent's components. If it fails, it's ignored as it will just
+	// be replaced with the correct contents from the clients checkin.
+	var agentComponents []model.ComponentsItems
+	if err := json.Unmarshal(agent.Components, &agentComponents); err != nil {
+		zlog.Warn().Err(err).Msg("components in document invalid; ignoring it")
+	}
+
 	var outComponents []byte
 
 	// Compare the deserialized meta structures and return the bytes to update if different
+<<<<<<< HEAD
 	if !reflect.DeepEqual(reqComponents, agent.Components) {
 
 		reqComponentsJSON, _ := json.Marshal(*req.Components)
+=======
+	if !reflect.DeepEqual(reqComponents, agentComponents) {
+		reqComponentsJSON, _ := json.Marshal(req.Components)
+>>>>>>> f9dbe41 (Handle malformatted JSON in `.fleet-agents` `components` field (#5858))
 		zlog.Trace().
-			Str("oldComponents", string(agentComponentsJSON)).
+			Str("oldComponents", string(agent.Components)).
 			Str("req.Components", string(reqComponentsJSON)).
 			Msg("local components data is not equal")
 
