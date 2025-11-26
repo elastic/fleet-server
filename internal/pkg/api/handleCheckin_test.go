f0579c0ba29e304bbd51a776011fd18f4623942e
Michal <michal.pristas@elastic.co>
2025-11-26 09:52:24 +0100

    [refactor] removed unused 'upgrade_status' field

--- internal/pkg/api/handleCheckin_test.go
+++ internal/pkg/api/handleCheckin_test.go
@@ -419,7 +419,7 @@ func TestProcessUpgradeDetails(t *testing.T) {
 					t.Logf("bulk match unmarshal error: %v", err)
 					return false
 				}
-				return doc.Doc[dl.FieldUpgradeDetails] == nil && doc.Doc[dl.FieldUpgradeStartedAt] == nil && doc.Doc[dl.FieldUpgradeStatus] == nil && doc.Doc[dl.FieldUpgradedAt] != ""
+				return doc.Doc[dl.FieldUpgradeDetails] == nil && doc.Doc[dl.FieldUpgradeStartedAt] == nil && doc.Doc[dl.FieldUpgradedAt] != ""
 			}), mock.Anything, mock.Anything).Return(nil)
 			return mBulk
 		},
