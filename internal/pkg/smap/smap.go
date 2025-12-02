54789cf41561aba96d1baba045a9f7836df07346
Shaunak Kashyap <ycombinator@gmail.com>
2025-12-01 17:08:47 -0800

    Add smap.Get and smap.Set methods

--- internal/pkg/smap/smap.go
+++ internal/pkg/smap/smap.go
@@ -11,6 +11,8 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"strings"
 )
 
 type Map map[string]interface{}
@@ -69,6 +71,118 @@ func (m Map) Marshal() ([]byte, error) {
 	return json.Marshal(m)
 }
 
+// Get returns the value at the specified key path
+// For example:
+// m.Get("a.b.c") => m["a"]["b"]["c"]
+// m.Get("a.2") => m["a"][2]
+// m.Get("a.2.c") => m["a"][2]["c"]
+func (m Map) Get(keyPath string) any {
+	if m == nil {
+		return nil
+	}
+
+	var curr any = m
+	var key string
+	var index uint
+	var isIndex bool
+
+	parts := strings.Split(keyPath, ".")
+	for _, part := range parts {
+		key = part
+
+		// Check if part is an index
+		index, isIndex = parseIndex(part)
+
+		// Traverse to the next level
+		if isIndex {
+			sCurr, ok := curr.([]any)
+			if !ok {
+				return nil
+			}
+			if index < 0 || int(index) >= len(sCurr) {
+				return nil
+			}
+			curr = sCurr[index]
+		} else {
+			mCurr, ok := isSMap(curr)
+			if !ok {
+				return nil
+			}
+			curr = mCurr[key]
+		}
+	}
+
+	return curr
+}
+
+// Set sets the value at the specified key path.
+// For example:
+// m.Set("a.b.c", value) => m["a"]["b"]["c"] = value
+// m.Set("a.2", value) => m["a"][2] = value
+// m.Set("a.2.c", value) => m["a"][2]["c"] = value
+func (m Map) Set(keyPath string, value any) error {
+	if m == nil {
+		return nil
+	}
+
+	var curr any = m
+	var parent any
+	var key string
+	var index uint
+	var isIndex bool
+
+	parts := strings.Split(keyPath, ".")
+	for i, part := range parts {
+		key = part
+		parent = curr
+		parentPath := strings.Join(parts[:i], ".")
+
+		// Check if part is an index
+		index, isIndex = parseIndex(part)
+
+		// If last part, set the value
+		if i == len(parts)-1 {
+			if isIndex {
+				sParent, ok := parent.([]any)
+				if !ok {
+					return fmt.Errorf("expected slice at %s, got %T", parentPath, parent)
+				}
+				if index < 0 || int(index) >= len(sParent) {
+					return fmt.Errorf("index out of bounds at %s: %d", parentPath, index)
+				}
+				sParent[index] = value
+			} else {
+				mParent, ok := isSMap(parent)
+				if !ok {
+					return fmt.Errorf("expected map at %s, got %T", parentPath, parent)
+				}
+				mParent[key] = value
+			}
+			return nil
+		}
+
+		// Traverse to the next level
+		if isIndex {
+			sCurr, ok := curr.([]any)
+			if !ok {
+				return fmt.Errorf("expected slice at %s, got %T", parentPath, curr)
+			}
+			if index < 0 || int(index) >= len(sCurr) {
+				return fmt.Errorf("index out of bounds at %s: %d", parentPath, index)
+			}
+			curr = sCurr[index]
+		} else {
+			mCurr, ok := isSMap(curr)
+			if !ok {
+				return fmt.Errorf("expected map at %s, got %T", parentPath, curr)
+			}
+			curr = mCurr[key]
+		}
+	}
+
+	return nil
+}
+
 // Parse generates a Map from the passed data.
 // data is assumed to be a json object.
 // TODO Should we refactor this to UnmarshalJSON?
@@ -82,4 +196,25 @@ func Parse(data []byte) (Map, error) {
 	err := json.Unmarshal(data, &m)
 
 	return m, err
+}
+
+func parseIndex(str string) (uint, bool) {
+	// Try to read str as an integer
+	var index uint
+	if _, err := fmt.Sscanf(str, "%d", &index); err != nil {
+		return 0, false
+	}
+	return index, true
+}
+
+func isSMap(v any) (map[string]any, bool) {
+	if mapVal, ok := v.(map[string]any); ok {
+		return mapVal, true
+	}
+
+	if mapVal, ok := v.(Map); ok {
+		return mapVal, true
+	}
+
+	return nil, false
 }
---
+++ internal/pkg/smap/smap_test.go
@@ -0,0 +1,102 @@
+package smap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGet(t *testing.T) {
+	tests := map[string]struct {
+		input    Map
+		keyPath  string
+		expected any
+	}{
+		"a": {
+			input:    Map{"a": 1},
+			keyPath:  "a",
+			expected: 1,
+		},
+		"a.b": {
+			input:    Map{"a": Map{"b": 2}},
+			keyPath:  "a.b",
+			expected: 2,
+		},
+		"a.1": {
+			input:    Map{"a": []any{10, 20, 30}},
+			keyPath:  "a.1",
+			expected: 20,
+		},
+		"a.b.2.c": {
+			input:    Map{"a": Map{"b": []any{1, "b", map[string]any{"c": 3}}}},
+			keyPath:  "a.b.2.c",
+			expected: 3,
+		},
+		"nonexistent": {
+			input:    Map{"a": 1},
+			keyPath:  "b",
+			expected: nil,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := tc.input.Get(tc.keyPath)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestSet(t *testing.T) {
+	tests := map[string]struct {
+		input       Map
+		keyPath     string
+		value       any
+		expected    Map
+		expectedErr string
+	}{
+		"a": {
+			input:    Map{"a": 1},
+			keyPath:  "b",
+			value:    42,
+			expected: Map{"a": 1, "b": 42},
+		},
+		"a.c": {
+			input:    Map{"a": Map{"b": map[string]any{"d": 1}}},
+			keyPath:  "a.c",
+			value:    42,
+			expected: Map{"a": Map{"b": map[string]any{"d": 1}, "c": 42}},
+		},
+		"a.1": {
+			input:    Map{"a": []any{10, 20, 30}},
+			keyPath:  "a.1",
+			value:    42,
+			expected: Map{"a": []any{10, 42, 30}},
+		},
+		"a.3": {
+			input:       Map{"a": []any{10, 20, 30}},
+			keyPath:     "a.3",
+			value:       42,
+			expected:    Map{"a": []any{10, 20, 30}}, // No change, index out of bounds
+			expectedErr: "index out of bounds at a: 3",
+		},
+		"a.b.2.c": {
+			input:    Map{"a": Map{"b": []any{1, "b", map[string]any{"c": 3}}}},
+			keyPath:  "a.b.2.c",
+			value:    42,
+			expected: Map{"a": Map{"b": []any{1, "b", map[string]any{"c": 42}}}},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := tc.input.Set(tc.keyPath, tc.value)
+			if tc.expectedErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.Equal(t, tc.expectedErr, err.Error())
+			}
+			require.Equal(t, tc.expected, tc.input)
+		})
+	}
+}
