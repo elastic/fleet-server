f026157f4377ba64cbe67c70f7e02ce2b1753b6c
Paolo Chila <paolo.chila@elastic.co>
2025-12-11 07:52:32 +0100

    fixup! Move rollbacks under upgrade/rollbacks in ES agent document

--- internal/pkg/checkin/bulk.go
+++ internal/pkg/checkin/bulk.go
@@ -370,17 +370,11 @@ func toUpdateBody(now string, pending pendingT) ([]byte, error) {
 		}
 
 		if pending.extra.availableRollbacks != nil {
-			if _, ok := fields[dl.FieldUpgradeInfo]; !ok {
-				fields[dl.FieldUpgradeInfo] = bulk.UpdateFields{}
-			}
-
 			if upgradeInfo, ok := fields[dl.FieldUpgradeInfo].(bulk.UpdateFields); ok {
 				upgradeInfo[dl.FieldAvailableRollbacks] = json.RawMessage(pending.extra.availableRollbacks)
 			} else {
-				// return an error as we have an unexpected value for the upgrade info container
-				return nil, fmt.Errorf("unexpected value %T for key %s in agent document", fields[dl.FieldUpgradeInfo], dl.FieldUpgradeInfo)
+				fields[dl.FieldUpgradeInfo] = bulk.UpdateFields{dl.FieldAvailableRollbacks: json.RawMessage(pending.extra.availableRollbacks)}
 			}
-
 		}
 	}
 	return fields.Marshal()
