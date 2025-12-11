8c83448a08c98b099a9f01abf8fc4ca500143fcd
elasticmachine <infra-root+elasticmachine@elastic.co>
2025-12-11 20:13:39 +0000

    [Release] update version

--- version/version.go
+++ version/version.go
@@ -6,4 +6,4 @@ package version
 
 // DefaultVersion is the current release version of Fleet-server, this version must match the
 // Elastic Agent version.
-const DefaultVersion = "9.1.9"
+const DefaultVersion = "9.1.10"
