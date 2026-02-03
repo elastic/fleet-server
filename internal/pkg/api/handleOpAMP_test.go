47892150e703f9fe63e8bcc526fbbefcc69e4b94
Shaunak Kashyap <ycombinator@gmail.com>
2026-02-03 15:37:01 -0800

    Adding unit test for feature flag

---
+++ internal/pkg/api/handleOpAMP_test.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/elastic/fleet-server/v7/internal/pkg/apikey"
+	"github.com/elastic/fleet-server/v7/internal/pkg/config"
+	testlog "github.com/elastic/fleet-server/v7/internal/pkg/testing/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFeatureFlag(t *testing.T) {
+	cases := map[string]struct {
+		FeatureFlagEnabled bool
+		WantError          error
+	}{
+		"feature flag is disabled": {
+			FeatureFlagEnabled: false,
+			WantError:          ErrOpAMPDisabled,
+		},
+		"feature flag is enabled": {
+			FeatureFlagEnabled: true,
+			WantError:          apikey.ErrNoAuthHeader,
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			cfg := &config.Server{
+				Features: config.FeatureFlags{
+					EnableOpAMP: tc.FeatureFlagEnabled,
+				},
+			}
+
+			logger := testlog.SetLogger(t)
+			req := http.Request{}
+			var resp http.ResponseWriter
+
+			oa := OpAMPT{cfg: cfg}
+			err := oa.handleOpAMP(logger, &req, resp)
+			require.Equal(t, tc.WantError, err)
+		})
+	}
+}
