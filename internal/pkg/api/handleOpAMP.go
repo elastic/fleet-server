ff778a4a0607a55d8f8d50a055bc55c6ad55e68c
Shaunak Kashyap <ycombinator@gmail.com>
2026-02-11 15:53:45 -0800

    Update internal/pkg/api/handleOpAMP.go
    Co-authored-by: Michel Laterman <82832767+michel-laterman@users.noreply.github.com>

--- internal/pkg/api/handleOpAMP.go
+++ internal/pkg/api/handleOpAMP.go
@@ -145,7 +145,7 @@ func (oa *OpAMPT) handleMessage(zlog zerolog.Logger, apiKey *apikey.APIKey) func
 				InstanceUid: instanceUID.Bytes(),
 				ErrorResponse: &protobufs.ServerErrorResponse{
 					Type:         protobufs.ServerErrorResponseType_ServerErrorResponseType_Unavailable,
-					ErrorMessage: fmt.Errorf("failed to check if agent is enrolled: %w", err).Error(),
+					ErrorMessage: fmt.Sprintf("failed to check if agent is enrolled: %v", err)
 				},
 			}
 		}
