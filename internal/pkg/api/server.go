df9cb16d08a0563e8074bca47f8bcedd00197726
Shaunak Kashyap <ycombinator@gmail.com>
2026-02-11 16:17:07 -0800

    Log when enabling OpAMP endpoint

--- internal/pkg/api/server.go
+++ internal/pkg/api/server.go
@@ -56,6 +56,7 @@ func NewServer(addr string, cfg *config.Server, opts ...APIOpt) *server {
 	// let OpAMP server modify connection context (setup later when HTTP server
 	// object is constructed).
 	if a.oa != nil && a.oa.Enabled() {
+		zerolog.Log().Info().Msg("enabling OpAMP endpoint")
 		handler = addOpAMPRouteHandler(handler, a.oa, &cfg.Limits)
 		s.connContext = a.oa.connCtx
 	}
