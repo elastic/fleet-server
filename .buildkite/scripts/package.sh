37dd54621aabb5daa8503891d1ee8b06fe117e94
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2026-06-23 10:33:03 -0700

    Remove type from package.sh (#7277)
    Remove the last uses of the type arg from the package.sh script and
    use SNAPSHOT env var to determine artifact upload location.

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -6,7 +6,7 @@ source .buildkite/scripts/common.sh
 
 readonly VERSION_QUALIFIER="${VERSION_QUALIFIER:-""}"
 
-if [[ ${BUILDKITE_BRANCH} == "main" && ${TYPE} == "staging" && -z ${VERSION_QUALIFIER} ]]; then
+if [[ ${BUILDKITE_BRANCH} == "main" && -z ${VERSION_QUALIFIER} ]]; then
     echo "INFO: staging artifacts for the main branch are not required."
     exit 0
 fi
@@ -16,4 +16,4 @@ with_go
 with_mage
 
 mage docker:release
-upload_mbp_packages_to_gcp_bucket "build/distributions/**/*" "${TYPE}"
+upload_mbp_packages_to_gcp_bucket "build/distributions/**/*"
