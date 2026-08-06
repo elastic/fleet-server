24351f468ec53760759c8f5dcd0519ac2eb3f794
Shaunak Kashyap <ycombinator@gmail.com>
2026-08-06 09:53:36 -0700

    bulk: remove spurious extra space in constant comment

--- internal/pkg/bulk/engine.go
+++ internal/pkg/bulk/engine.go
@@ -140,7 +140,7 @@ const (
 	defaultAPIKeyMaxParallel              = 32
 	defaultApikeyMaxReqSize               = 100 * 1024 * 1024
 	defaultFlushContextTimeout            = time.Minute * 1
-	defaultMaxPendingBulkDispatches int64 = 0  // 0 means no limit
+	defaultMaxPendingBulkDispatches int64 = 0 // 0 means no limit
 	defaultMaxConcurrentSecretReads int64 = 32
 
 	// dispatchAbortDrainTimeout bounds how long the drain helper waits for
