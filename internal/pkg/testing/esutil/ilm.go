b8dc5d27760bd4187fe4f45dd8e9eedd3a768115
Shaunak Kashyap <ycombinator@gmail.com>
2026-08-11 15:55:22 -0700

    fix: support go-elasticsearch v9 API

--- internal/pkg/testing/esutil/ilm.go
+++ internal/pkg/testing/esutil/ilm.go
@@ -150,8 +150,9 @@ func createILMPolicy(ctx context.Context, cli *elasticsearch.Client, name string
 	// in that case let's just create the ILM policy
 	zerolog.Ctx(ctx).Debug().Str("policy", policy).Str("body", body).Msg("Creating ILM policy")
 
-	res, err := cli.ILM.PutLifecycle(policy,
-		cli.ILM.PutLifecycle.WithBody(strings.NewReader(body)),
+	res, err := cli.ILM.PutLifecycle(
+		strings.NewReader(body),
+		policy,
 		cli.ILM.PutLifecycle.WithContext(ctx),
 	)
 	if err != nil {
